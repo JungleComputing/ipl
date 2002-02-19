df6f5b9a75a9de97ef1cbf29bf5154498507e99f
Jason Maassen <j.maassen@esciencecenter.nl>
2002-02-19 14:25:52 +0000

    Fixed a byte ordering bug (different byte orders used between Java type[] -> byte[] and native type[] -> byte[]).
    Jason

--- src/ibis/io/Conversion.java
+++ src/ibis/io/Conversion.java
@@ -89,8 +89,8 @@ class Conversion {
 			
 			for (int i=0;i<len;i++) { 			
 				temp = src[off+i];
-				dst[count++] = (byte) ((temp >> 8)  & 0xff);
 				dst[count++] = (byte) (temp & 0xff);
+				dst[count++] = (byte) ((temp >> 8)  & 0xff);
 			} 
 		} else { 
 			n_short2byte(src, off, len, dst, off2);
@@ -106,12 +106,10 @@ class Conversion {
 			
 			for (int i=0;i<len;i++) { 			
 				temp = src[off+i];
-				dst[count++] = (byte) ((temp >> 24) & 0xff);
-				dst[count++] = (byte) ((temp >> 16) & 0xff);
-				dst[count++] = (byte) ((temp >> 8)  & 0xff);
 				dst[count++] = (byte) (temp & 0xff);
-				
-//				System.out.println("converted int(" + temp + ") to bytes");
+				dst[count++] = (byte) ((temp >> 8)  & 0xff);
+				dst[count++] = (byte) ((temp >> 16) & 0xff);
+				dst[count++] = (byte) ((temp >> 24) & 0xff);
 			} 
 
 		} else { 
@@ -128,14 +126,14 @@ class Conversion {
 			
 			for (int i=0;i<len;i++) { 			
 				temp = src[off+i];
-				dst[count++] = (byte)(0xff & (temp >> 56));
-				dst[count++] = (byte)(0xff & (temp >> 48));
-				dst[count++] = (byte)(0xff & (temp >> 40));
-				dst[count++] = (byte)(0xff & (temp >> 32));
-				dst[count++] = (byte)(0xff & (temp >> 24));
-				dst[count++] = (byte)(0xff & (temp >> 16));
-				dst[count++] = (byte)(0xff & (temp >> 8));
 				dst[count++] = (byte)(0xff & temp);
+				dst[count++] = (byte)(0xff & (temp >> 8));
+				dst[count++] = (byte)(0xff & (temp >> 16));
+				dst[count++] = (byte)(0xff & (temp >> 24));
+				dst[count++] = (byte)(0xff & (temp >> 32));
+				dst[count++] = (byte)(0xff & (temp >> 40));
+				dst[count++] = (byte)(0xff & (temp >> 48));
+				dst[count++] = (byte)(0xff & (temp >> 56));
 			} 
 		} else { 
 			n_long2byte(src, off, len, dst, off2);
@@ -151,10 +149,10 @@ class Conversion {
 			
 			for (int i=0;i<len;i++) { 			
 				temp = Float.floatToIntBits(src[off+i]);
-				dst[count++] = (byte) ((temp >> 24) & 0xff);
-				dst[count++] = (byte) ((temp >> 16) & 0xff);
-				dst[count++] = (byte) ((temp >> 8)  & 0xff);
 				dst[count++] = (byte) (temp & 0xff);
+				dst[count++] = (byte) ((temp >> 8)  & 0xff);
+				dst[count++] = (byte) ((temp >> 16) & 0xff);
+				dst[count++] = (byte) ((temp >> 24) & 0xff);
 			} 
 		} else { 
 			n_float2byte(src, off, len, dst, off2);
@@ -170,132 +168,134 @@ class Conversion {
 			
 			for (int i=0;i<len;i++) { 			
 				temp = Double.doubleToLongBits(src[off+i]);
-				dst[count++] = (byte)(0xff & (temp >> 56));
-				dst[count++] = (byte)(0xff & (temp >> 48));
-				dst[count++] = (byte)(0xff & (temp >> 40));
-				dst[count++] = (byte)(0xff & (temp >> 32));
-				dst[count++] = (byte)(0xff & (temp >> 24));
-				dst[count++] = (byte)(0xff & (temp >> 16));
-				dst[count++] = (byte)(0xff & (temp >> 8));
 				dst[count++] = (byte)(0xff & temp);
+				dst[count++] = (byte)(0xff & (temp >> 8));
+				dst[count++] = (byte)(0xff & (temp >> 16));
+				dst[count++] = (byte)(0xff & (temp >> 24));
+				dst[count++] = (byte)(0xff & (temp >> 32));
+				dst[count++] = (byte)(0xff & (temp >> 40));
+				dst[count++] = (byte)(0xff & (temp >> 48));
+				dst[count++] = (byte)(0xff & (temp >> 56));
 			} 
 		} else { 
 			n_double2byte(src, off, len, dst, off2);
 		}
 	}
 
-	public static final void byte2boolean(byte[] src, int off2, boolean[] dst, int off, int len) { 
+	public static final void byte2boolean(byte[] src, int index_src, boolean[] dst, int index_dst, int len) { 
 
 		if (len < BYTE2BOOLEAN_THRESHOLD) { 
 			for (int i=0;i<len;i++) { 			
-				dst[off+i] = (src[i] == 1);
+				dst[index_dst+i] = (src[i] == 1);
 			}
 		} else { 			
-			n_byte2boolean(src, off2, dst, off, len);
+			n_byte2boolean(src, index_src, dst, index_dst, len);
 		} 
 	} 
 
-	public static final void byte2short(byte[] src, int off2, short[] dst, int off, int len) {
+	public static final void byte2short(byte[] src, int index_src, short[] dst, int index_dst, int len) {
 
 		if (len < BYTE2SHORT_THRESHOLD) { 
-			int count = off2;
+			int count = index_src;
 			for (int i=0;i<len;i++) { 			
-				dst[off+i] = (short) (((short)(src[count++] & 0xff) <<  8) | 
-						      (short)(src[count++] & 0xff));
+				dst[index_dst+i] = (short) 
+					((short)(src[count++] & 0xff) | 
+					((short)(src[count++] & 0xff) << 8));
 			}
 		} else { 			
-			n_byte2short(src, off2, dst, off, len);
+			n_byte2short(src, index_src, dst, index_dst, len);
 		} 
 	}
 
-	public static final void byte2char(byte[] src, int off2, char[] dst, int off, int len) {
+	public static final void byte2char(byte[] src, int index_src, char[] dst, int index_dst, int len) {
 		
 		if (len < BYTE2CHAR_THRESHOLD) { 
-			int count = off2;
+			int count = index_src;
 			for (int i=0;i<len;i++) { 			
-				dst[off+i] = (char)(((char)(src[count++] & 0xff) <<  8) |
-						    (char)(src[count++] & 0xff));
+				dst[index_dst+i] = (char) 
+					 ((char)(src[count++] & 0xff) |
+					 ((char)(src[count++] & 0xff) << 8));
 			}
 		} else { 			
-			n_byte2char(src, off2, dst, off, len);
+			n_byte2char(src, index_src, dst, index_dst, len);
 		}
 	}
 
-	public static final void byte2int(byte[] src, int off2, int[] dst, int off, int len) {
+	public static final void byte2int(byte[] src, int index_src, int[] dst, int index_dst, int len) {
 
 		if (len < BYTE2INT_THRESHOLD) { 
-			int count = off2;
+			int count = index_src;
 			for (int i=0;i<len;i++) { 			
-				dst[off+i] = (((int)(src[count++] & 0xff) << 24) |
-					      ((int)(src[count++] & 0xff) << 16) |
-					      ((int)(src[count++] & 0xff) <<  8) |
-					      (int)(src[count++] & 0xff));
-
-//				System.out.println("converted bytes to int(" + dst[off+i]  + ")");
+				dst[index_dst+i] = (int)
+					((int)(src[count++] & 0xff) | 
+					((int)(src[count++] & 0xff) <<  8) |
+					((int)(src[count++] & 0xff) << 16) |
+					((int)(src[count++] & 0xff) << 24));
 			}
 		} else { 			
-			n_byte2int(src, off2, dst, off, len);
+			n_byte2int(src, index_src, dst, index_dst, len);
 		}
 	}
 
-	public static final void byte2long(byte[] src, int off2, long[] dst, int off, int len) { 		
+	public static final void byte2long(byte[] src, int index_src, long[] dst, int index_dst, int len) { 		
 
 		if (len < BYTE2LONG_THRESHOLD) {
-			int count = off2;
+			int count = index_src;
 			
 			for (int i=0;i<len;i++) { 			
-				dst[off+i] = (((long)(src[count++] & 0xff) << 56) |
-					      ((long)(src[count++] & 0xff) << 48) |
-					      ((long)(src[count++] & 0xff) << 40) |
-					      ((long)(src[count++] & 0xff) << 32) |
-					      ((long)(src[count++] & 0xff) << 24) |
-					      ((long)(src[count++] & 0xff) << 16) |
-					      ((long)(src[count++] & 0xff) <<  8) |
-					      (long)(src[count++] & 0xff));
+				dst[index_dst+i] = (long)
+					(((long)(src[count++] & 0xff))        |
+					 ((long)(src[count++] & 0xff) << 8)   |
+					 ((long)(src[count++] & 0xff) << 16)  |
+					 ((long)(src[count++] & 0xff) << 24)  |
+					 ((long)(src[count++] & 0xff) << 32)  |
+					 ((long)(src[count++] & 0xff) << 40)  |
+					 ((long)(src[count++] & 0xff) << 48)  |
+					 ((long)(src[count++] & 0xff) << 56));
 			}
 		} else { 			
-			n_byte2long(src, off2, dst, off, len);
+			n_byte2long(src, index_src, dst, index_dst, len);
 		}
 	} 
 
-	public static final void byte2float(byte[] src, int off2, float[] dst, int off, int len) { 
+	public static final void byte2float(byte[] src, int index_src, float[] dst, int index_dst, int len) { 
 
 		if (len < BYTE2FLOAT_THRESHOLD) {
 			int temp = 0;
-			int count = off2;
+			int count = index_src;
 			
 			for (int i=0;i<len;i++) { 			
-				temp =	((int)(src[count++] & 0xff) << 24) |
+				temp =	((int)(src[count++] & 0xff))       |
+					((int)(src[count++] & 0xff) << 8)  |
 					((int)(src[count++] & 0xff) << 16) |
-					((int)(src[count++] & 0xff) <<  8) |
-					(int)(src[count++] & 0xff);
-				dst[off+i] = Float.intBitsToFloat(temp);
+					((int)(src[count++] & 0xff) << 24);
+				dst[index_dst+i] = Float.intBitsToFloat(temp);
 			}
 		} else { 
-			n_byte2float(src, off2, dst, off, len);
+			n_byte2float(src, index_src, dst, index_dst, len);
 		}
 	} 
 
-	public static final void byte2double(byte[] src, int off2, double[] dst, int off, int len) { 
+	public static final void byte2double(byte[] src, int index_src, double[] dst, int index_dst, int len) { 
 
 		if (len < BYTE2DOUBLE_THRESHOLD) {
 			
 			long temp = 0;
-			int count = off2;
+			int count = index_src;
 			
 			for (int i=0;i<len;i++) { 			
-				temp = ((long)(src[count++] & 0xff) << 56) |
-					((long)(src[count++] & 0xff) << 48) |
-					((long)(src[count++] & 0xff) << 40) |
-					((long)(src[count++] & 0xff) << 32) |
-					((long)(src[count++] & 0xff) << 24) |
+				temp = ((long)(src[count++] & 0xff)        |
+					((long)(src[count++] & 0xff) << 8)  |
 					((long)(src[count++] & 0xff) << 16) |
-					((long)(src[count++] & 0xff) <<  8) |
-					(long)(src[count++] & 0xff);
-				dst[off+i] = Double.longBitsToDouble(temp);
+					((long)(src[count++] & 0xff) << 24) |
+					((long)(src[count++] & 0xff) << 32) |
+					((long)(src[count++] & 0xff) << 40) |
+					((long)(src[count++] & 0xff) << 48) |
+					((long)(src[count++] & 0xff) << 56));
+				dst[index_dst+i] = Double.longBitsToDouble(temp);
 			}
 		} else { 
-			n_byte2double(src, off2, dst, off, len);
+			n_byte2double(src, index_src, dst, index_dst, len);
 		}			
 	} 
 } 
