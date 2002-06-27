def8a8ac6a289bd7677e03c69872e17d7513ee6e
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2002-06-27 08:34:44 +0000

    Now generate scripts in bin using the configuration. Ibisc can now compile Ibis itself. IbisIdentifier now is an abstract class, it contains a name and address. The address is needed for the name server. Because it contains fields, it must be rewritten with the IO generator. TcpIbisIdentifier was overwritten with generated code *again*, fixed.
    --Rob

--- src/ibis/impl/tcp/TcpIbisIdentifier.java
+++ src/ibis/impl/tcp/TcpIbisIdentifier.java
@@ -1,3 +1,4 @@
+// handcrafted by Rob, do *NOT* overwrite!
 package ibis.ipl.impl.tcp;
 
 import java.io.DataInputStream;
@@ -7,29 +8,31 @@ import java.io.EOFException;
 import java.net.InetAddress;
 import ibis.ipl.IbisIdentifier;
 import ibis.io.Serializable;
-import ibis.ipl.IbisIOException;
 import ibis.io.MantaOutputStream;
 import ibis.io.MantaInputStream;
 
-
+// the implements should be unnecessary, but the IOGenerator soes not 
+// see that the super class implents it, and rewrites the bytecode.
 public final class TcpIbisIdentifier extends IbisIdentifier implements java.io.Serializable, ibis.io.Serializable {
 	public static final int serialversionID = 1;
-	// inet addresses  are transferred as strings in the form WWW.XXX.YYY.ZZZ
-        // to avoid (SUN) serialization problems, *NO* lookups are needed.
 
-	public TcpIbisIdentifier(String name, InetAddress address) { 
+	public TcpIbisIdentifier() {
+		System.err.println("EEK, TcpIbisIdentifier default ctor!");
+	}
+
+	public TcpIbisIdentifier(String name, InetAddress address) {
 		super(name, address);
 	}
 
-	public TcpIbisIdentifier(MantaInputStream stream) throws IbisIOException, ClassNotFoundException {
-		super(null, null);
+	public TcpIbisIdentifier(MantaInputStream stream) throws ibis.ipl.IbisIOException, ClassNotFoundException {
 		stream.addObjectToCycleCheck(this);
 		int handle = stream.readInt();
 		if(handle < 0) {
 			try {
-				address = InetAddress.getByName(stream.readUTF());
+				address = InetAddress.getByName(stream.readUTF()); // this does not do a real lookup
 			} catch (Exception e) {
-				throw new IbisIOException(e);
+				System.err.println("EEK, could not create an inet address from a IP address. This should not happen");
+				System.exit(1);
 			}
 			name = stream.readUTF();
 			TcpIbis.globalIbis.identTable.addIbis(stream, -handle, this);
@@ -37,15 +40,6 @@ public final class TcpIbisIdentifier extends IbisIdentifier implements java.io.S
 			TcpIbisIdentifier ident = (TcpIbisIdentifier) TcpIbis.globalIbis.identTable.getIbis(stream, handle);
 			address = ident.address;
 			name = ident.name;
-		}
-	}
-
-	public final void generated_WriteObject(MantaOutputStream stream) throws IbisIOException {
-		int handle = TcpIbis.globalIbis.identTable.getHandle(stream, this);
-		stream.writeInt(handle);
-		if(handle < 0) { // First time, send it.
-			stream.writeUTF(address.getHostAddress());
-			stream.writeUTF(name);
 		}
 	}
 
@@ -64,12 +58,19 @@ public final class TcpIbisIdentifier extends IbisIdentifier implements java.io.S
 	}
 
 	public String toString() {
-		return ("(TcpId: " + name + " on [" + 
-			address.getHostName() + ", " + 
-			address.getHostAddress() + "])");
+		return ("(TcpId: " + name + " on [" + address.getHostName() + ", " + address.getHostAddress() + "])");
 	}
 
 	public int hashCode() {
 		return name.hashCode();
+	}
+
+	public final void generated_WriteObject(MantaOutputStream stream) throws ibis.ipl.IbisIOException {
+		int handle = TcpIbis.globalIbis.identTable.getHandle(stream, this);
+		stream.writeInt(handle);
+		if(handle < 0) { // First time, send it.
+			stream.writeUTF(address.getHostAddress());
+			stream.writeUTF(name);
+		}
 	}
 }
