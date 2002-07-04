68bdc1330f2cf7442c0e861fdc27b3e9789a75ae
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2002-07-04 15:02:46 +0000

    Added run_ibis script, to make starting ibis apps easier. Added rewrite target to Makefile that rewrites the class libraries Fixed io generator to be able to rewrite class libs.

--- apps/satin/config.mk
+++ apps/satin/config.mk
@@ -15,7 +15,8 @@ APPS += mmult
 #APPS += checkers
 
 RUNJAVA = /home3/rob/bin/runjava_ibis_ibm
-SIZES = 64 48 32 24 16 8 4 2 1
+#SIZES = 64 48 32 24 16 8 4 2 1
+SIZES = 4
 
 PAR_SCHEDULING  =
 PAR_SCHEDULING += RS
@@ -33,4 +34,4 @@ EXCLUDES =
 
 SEQ_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -rf $(SEQ_OUT_FILE); prun $(EXCLUDES) -1 -o $(SEQ_OUT_FILE) $(SEQ_TIME) -v $(RUNJAVA) 1 $(MAIN_CLASS_NAME) $(APP_OPTIONS) $(SEQ_RUN_OPTIONS) < /dev/null; mv $(SEQ_OUT_FILE).0 $(SEQ_OUT_FILE))
 
-PAR_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -f $(PAR_OUT_FILE).*; prun $(EXCLUDES) -1 -o $(PAR_OUT_FILE) $(PAR_TIME) -v $(RUNJAVA) $$s $(MAIN_CLASS_NAME) $(APP_OPTIONS) $(PAR_RUN_OPTIONS) < /dev/null; cat $(PAR_OUT_FILE).* > $(PAR_OUT_FILE); rm -f $(PAR_OUT_FILE).*)
+PAR_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -f $(PAR_OUT_FILE).*; prun $(EXCLUDES) -1 -o $(PAR_OUT_FILE) $(PAR_TIME) -v $(RUNJAVA) $$s $(MAIN_CLASS_NAME) $(APP_OPTIONS) $(PAR_RUN_OPTIONS) < /dev/null; cat $(PAR_OUT_FILE).* > $(PAR_OUT_FILE); rm -f $(PAR_OUT_FILE).*) &
