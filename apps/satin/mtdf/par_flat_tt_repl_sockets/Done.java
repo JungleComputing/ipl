c52baf710aa55f9bce865424194e5b29a3b3e8ce
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2002-11-12 12:51:51 +0000

    added awari versions with replicated transposition tables, both with RMI and sockets

---
+++ apps/satin/mtdf/par_flat_tt_repl_sockets/Done.java
@@ -0,0 +1,13 @@
+final class Done extends Throwable {
+	short score;
+	short currChild;
+
+	Done(short score, short currChild) {
+		this.score = score;
+		this.currChild = currChild;
+	}
+
+	public Throwable fillInStackTrace() {
+		return null;
+	}
+}
