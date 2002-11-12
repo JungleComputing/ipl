31e0d1bef4ea167bd3349920be03b6aeb17ba877
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2002-11-12 12:54:03 +0000

    added more awari versions

---
+++ apps/satin/mtdf/par_prealloc/MtdfInterface.java
@@ -0,0 +1,3 @@
+public interface MtdfInterface extends ibis.satin.Spawnable {
+	public void spawn_depthFirstSearch(NodeType node, int pivot, int depth, short currChild) throws Done;
+}
---
+++ apps/satin/mtdf/par_prealloc/TranspositionTable.java
@@ -0,0 +1,49 @@
+final class TranspositionTable {
+
+	static final boolean SUPPORT_TT = true;
+
+	static final int SIZE = 1 << 22;
+	static int lookups = 0, hits = 0, sorts = 0, stores = 0, 
+		overwrites = 0, visited = 0, scoreImprovements = 0, cutOffs = 0;
+
+	TranspositionTableEntry[] entries = new TranspositionTableEntry[SIZE];
+
+	TranspositionTable() {
+		for(int i=0; i<SIZE; i++) {
+			entries[i] = new TranspositionTableEntry();
+		}
+	}
+
+	TranspositionTableEntry lookup(long signature) {
+		lookups++;
+		return entries[(int)signature & (SIZE-1)];
+	}
+
+	void store(long tag, short value, short bestChild, byte depth, boolean lowerBound) {
+		stores++;
+
+		if(!SUPPORT_TT) return;
+
+		int index = (int)tag & (SIZE-1);
+		TranspositionTableEntry e = entries[index];
+
+		if(depth < e.depth) {
+			return;
+		}
+		overwrites++;
+
+		e.tag = tag;
+		e.value = value;
+		e.bestChild = bestChild;
+		e.depth = depth;
+		e.lowerBound = lowerBound;
+		entries[index] = e;
+	}
+
+	protected void finalize() throws Throwable {
+		System.out.println("tt: lookups: " + lookups + ", hits: " + hits + ", sorts: " + sorts +
+				   ", stores: " + stores + ", overwrites: " + overwrites + 
+				   ", score incs: " + scoreImprovements + 
+				   ", cutoffs: " + cutOffs + ", visited: " + visited);
+	}
+}
