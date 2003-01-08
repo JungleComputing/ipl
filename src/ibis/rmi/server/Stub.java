4c793a182c7cfdfc0b7936674e65be2d5b6a71dd
Gosia Wrezinska <gosia@cs.vu.nl>
2003-01-08 13:08:51 +0000

    added some code to make a stub connect to the skeleton after being deserialized

--- src/ibis/rmi/server/Stub.java
+++ src/ibis/rmi/server/Stub.java
@@ -1,10 +1,12 @@
 package ibis.rmi.server;
 
 import ibis.ipl.*;
+import ibis.io.*;
+import ibis.rmi.*;
 
 public class Stub extends RemoteStub {
 
-    protected int stubID;
+    transient protected int stubID;
     transient protected SendPort send;
     transient protected ReceivePort reply;
     protected ReceivePortIdentifier skeletonPortId;
@@ -21,5 +23,39 @@ public class Stub extends RemoteStub {
     }
     
     //serialize & deserialize
-    
+
+    public void generated_WriteObject(MantaOutputStream mantaoutputstream)
+        throws IbisIOException
+    {
+        super.generated_WriteObject(mantaoutputstream);
+        mantaoutputstream.writeObject(skeletonPortId);
+    }
+
+    public Stub(MantaInputStream mantainputstream)
+        throws IbisIOException
+    {
+        super(mantainputstream);
+        skeletonPortId = (ReceivePortIdentifier)mantainputstream.readObject();
+
+	//after deserialization connect the stub to the skeleton	
+	send = RTS.createSendPort();
+	send.connect(skeletonPortId);
+	
+	reply = RTS.createReceivePort();
+	reply.enableConnections();
+	
+	WriteMessage wm = send.newMessage();
+	
+	wm.writeInt(-1);
+	wm.writeInt(0);
+	wm.writeObject(reply.identifier());
+	wm.send();
+	wm.finish();
+	
+	ReadMessage rm = reply.receive();
+	stubID = rm.readInt();
+	String stubType = (String) rm.readObject();
+	rm.finish();
+    }
+	
 }
