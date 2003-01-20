185030e51272562055c8c79c7286ca5e5705abcc
Jason Maassen <j.maassen@esciencecenter.nl>
2003-01-20 11:59:29 +0000

    Added 'readObject' method that sets up the link to the skeleton when the stub is de-serialized. This was not implemented for sun serialization yet.
    Jason

--- src/ibis/rmi/server/Stub.java
+++ src/ibis/rmi/server/Stub.java
@@ -57,5 +57,34 @@ public class Stub extends RemoteStub {
 	String stubType = (String) rm.readObject();
 	rm.finish();
     }
-	
+
+	private void readObject(java.io.ObjectInputStream in) throws java.io.IOException { 
+		try { 
+			in.defaultReadObject();
+		
+			send = RTS.createSendPort();
+			send.connect(skeletonPortId);
+			
+			reply = RTS.createReceivePort();
+			reply.enableConnections();
+			
+			WriteMessage wm = send.newMessage();
+			
+			wm.writeInt(-1);
+			wm.writeInt(0);
+			wm.writeObject(reply.identifier());
+			wm.send();
+			wm.finish();
+			
+			ReadMessage rm = reply.receive();
+			stubID = rm.readInt();
+			String stubType = (String) rm.readObject();
+			rm.finish();		
+		} catch (Exception e) { 
+			throw new java.io.IOException("EEK in readObject " + e);
+		} 
+	} 
 }
+
+
+
