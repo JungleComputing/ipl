9a32d7cadacc47a272cbbd09e0c22d19db031566
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2003-07-01 07:43:41 +0000

    *** empty log message ***

--- apps/satin/config.mk
+++ apps/satin/config.mk
@@ -30,17 +30,21 @@ PAR_OUT_FILE = output-$(OUT)-par-$$q-$$s
 SEQ_RUN_OPTIONS =
 PAR_RUN_OPTIONS  = -satin-stats 
 PAR_RUN_OPTIONS += -satin-closed
-#PAR_RUN_OPTIONS += -satin-ibis
-PAR_RUN_OPTIONS += -satin-panda
 
-SEQ_TIME = -asocial -t 5:0:0
-PAR_TIME = -asocial -t 1:0:0
+#PAR_RUN_OPTIONS += -satin-ibis
+#PAR_RUN_OPTIONS += -satin-panda
+#PAR_RUN_OPTIONS += -satin-no-upcalls
+#PAR_RUN_OPTIONS += -satin-upcall-polling
+#PAR_RUN_OPTIONS += -satin-alg CRS
+
+SEQ_TIME = -asocial -t 28:0:0
+PAR_TIME = -asocial -t 28:0:0
 
 EXCLUDES =
 
 NAME_SERVER = fs0.das2.cs.vu.nl
 
-NAMESERVER_PORT = 42042
+NAMESERVER_PORT = 4422
 
 DATE = `date '+%Y-%m-%d'`
 
@@ -50,4 +54,4 @@ DATE = `date '+%Y-%m-%d'`
 
 SEQ_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -f $(SEQ_OUT_FILE) $(SEQ_OUT_FILE).0; prun $(EXCLUDES) -1 -o $(SEQ_OUT_FILE) $(SEQ_TIME) -v $(IBIS_ROOT)/ibis/bin/run_ibis 1 $(NAMESERVER_PORT) $(NAME_SERVER) $(JVM_FLAGS) $(MAIN_CLASS_NAME) $(APP_OPTIONS) $(SEQ_RUN_OPTIONS) < /dev/null; mv $(SEQ_OUT_FILE).0 $(SEQ_OUT_FILE)) &
 
-PAR_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -f $(PAR_OUT_FILE) $(PAR_OUT_FILE).*; prun $(EXCLUDES) -o $(PAR_OUT_FILE) $(PAR_TIME) -v -1 $(IBIS_ROOT)/ibis/bin/run_ibis $$s $(NAMESERVER_PORT) $(NAME_SERVER) $(JVM_FLAGS) $(MAIN_CLASS_NAME) $(SMALL_APP_OPTIONS) $(PAR_RUN_OPTIONS) < /dev/null; )
+PAR_RUN_COMMAND = (/home/rob/bin/check_rsh_ports; rm -f $(PAR_OUT_FILE) $(PAR_OUT_FILE).*; prun $(EXCLUDES) -1 -o $(PAR_OUT_FILE) $(PAR_TIME) -v ~/projects/ibis/bin/run_ibis $$s $(NAMESERVER_PORT) $(NAME_SERVER) $(JVM_FLAGS) $(MAIN_CLASS_NAME) $(APP_OPTIONS) $(PAR_RUN_OPTIONS) < /dev/null; cat $(PAR_OUT_FILE).* > $(PAR_OUT_FILE); rm -f $(PAR_OUT_FILE).*) &
