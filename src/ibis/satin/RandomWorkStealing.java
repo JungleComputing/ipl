241e10bf742ee0a78f81251297a7026f12250bbf
Maik Nijhuis <maik@cs.vu.nl>
2003-11-24 10:21:35 +0000

    made readable, no code changes

--- src/ibis/satin/RandomWorkStealing.java
+++ src/ibis/satin/RandomWorkStealing.java
@@ -1,43 +1,43 @@
 package ibis.satin;
 
 final class RandomWorkStealing implements Algorithm {
-	Satin satin;
+    Satin satin;
 
-	RandomWorkStealing(Satin s) {
-		this.satin = s;
-	}
+    RandomWorkStealing(Satin s) {
+	this.satin = s;
+    }
 
-	public void clientIteration() {
-		InvocationRecord r;
-		Victim v;
+    public void clientIteration() {
+	InvocationRecord r;
+	Victim v;
 
-//		r = satin.q.getFromHead(); // Try the local queue
+//	r = satin.q.getFromHead(); // Try the local queue
 		
-//		if(r != null) {
-//			satin.callSatinFunction(r);
-//		} else {
-			/* We are idle. There is no work in the queue, and we are
-			   not running Java code. Try to steal a job. */
-			synchronized(satin) {
-				v = satin.victims.getRandomVictim();
-			}
-			satin.stealJob(v);
-//		}
-	}
-
-	public void stealReplyHandler(InvocationRecord ir, int opcode) {
+//	if(r != null) {
+//		satin.callSatinFunction(r);
+//	} else {
+		/* We are idle. There is no work in the queue, and we are
+		   not running Java code. Try to steal a job. */
 		synchronized(satin) {
-			satin.gotStealReply = true;
-			satin.stolenJob = ir;
-			satin.notifyAll();
+			v = satin.victims.getRandomVictim();
 		}
-	}	
+		satin.stealJob(v);
+//	}
+    }
 
-	public void exit() {
-		// Everything's synchronous, we don't have to wait for/do anything
+    public void stealReplyHandler(InvocationRecord ir, int opcode) {
+	synchronized(satin) {
+	    satin.gotStealReply = true;
+	    satin.stolenJob = ir;
+	    satin.notifyAll();
 	}
+    }	
 
-	public void printStats(java.io.PrintStream out) {
-		// Satin already prints everything..
-	}
+    public void exit() {
+	// Everything's synchronous, we don't have to wait for/do anything
+    }
+
+    public void printStats(java.io.PrintStream out) {
+	// Satin already prints everything..
+    }
 }
