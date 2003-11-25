2ddc0729d8a9307544aa9a00ede3d4ab95280855
Maik Nijhuis <maik@cs.vu.nl>
2003-11-25 10:26:21 +0000

    Merged some CVS conflicts; made readable again; node code changes

--- src/ibis/satin/SpawnCounter.java
+++ src/ibis/satin/SpawnCounter.java
@@ -2,6 +2,6 @@ package ibis.satin;
 
 /* Make this class final, so it can be inlined. */
 public final class SpawnCounter {
-	public volatile int value = 0;
-	SpawnCounter next;
+    public volatile int value = 0;
+    SpawnCounter next;
 }
