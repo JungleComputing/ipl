bd1d6ad25ec48643176f6e19a9f8a5ec3a402211
Rutger Hofman <rutger@cs.vu.nl>
2004-02-05 17:11:53 +0000

    Typo in import

--- src/ibis/io/Conversion.java
+++ src/ibis/io/Conversion.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import ibis.io.SimpleConversion;
 
 
 public abstract class Conversion { 
