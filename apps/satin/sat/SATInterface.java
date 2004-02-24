72bc9d0040feaa2d74e17918c6e2a9c4c143e36a
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-02-24 17:40:30 +0000

    CvR: + Ripped out agressive solver level, since it only slowed things down.

--- apps/satin/sat/SATInterface.java
+++ apps/satin/sat/SATInterface.java
@@ -8,5 +8,4 @@
 interface SATInterface extends ibis.satin.Spawnable
 {
     public void solve( int level, SATProblem p, SATContext ctx, int varix, boolean val ) throws SATResultException;
-    public void agressiveSolve( int level, SATProblem p, int var, boolean val ) throws SATResultException;
 }
--- apps/satin/sat/SATSolver.java
+++ apps/satin/sat/SATSolver.java
@@ -27,13 +27,6 @@ public class SATSolver extends ibis.satin.SatinObject implements SATInterface, j
      */
     static final int leafVariables = 0;
 
-    /** 
-     * For all combinations of the first `firstVariables' variables we
-     * apply simplification. This essentially creates 2^firstVariables
-     * sub-problems that are solved in their own context.
-     */
-    static final int firstVariables = 0;
-
     /**
      * A simple solver that is used when the remaining problem is too
      * small to justify expensive solvers.
@@ -158,7 +151,7 @@ public class SATSolver extends ibis.satin.SatinObject implements SATInterface, j
 	    return;
 	}
 
-	// TODO: when should we switch to the leaf solver?
+	// TODO: when (if ever) should we switch to the leaf solver?
 	if( false ){
 	    leafSolve( level+1, p, ctx.varlist, ctx.assignments, 0, false );
 	    leafSolve( level+1, p, ctx.varlist, ctx.assignments, 0, true );
@@ -167,108 +160,15 @@ public class SATSolver extends ibis.satin.SatinObject implements SATInterface, j
 	    // We have variable 'nextvar' to branch on.
 	    SATContext negctx = (SATContext) ctx.clone();
 	    SATContext posctx = (SATContext) ctx.clone();
-	    //if( ctx.posDominant( nextvar ) ){
 	    if( ctx.posDominant( nextvar ) ){
-		solve( level+1, p, negctx, nextvar, false );
-		solve( level+1, p, posctx, nextvar, true );
-	    }
-	    else {
 		solve( level+1, p, posctx, nextvar, true );
 		solve( level+1, p, negctx, nextvar, false );
 	    }
-	    sync();
-	}
-    }
-
-    /**
-     * Agressively solves the specified SAT problem by creating a
-     * specialized clone of the problem with the given variable
-     * assigned the given value.
-     * The method throws a SATResultException if it finds a solution,
-     * or terminates normally if it cannot find a solution.
-     * @param level the recursion level of the solution process
-     * @param p the SAT problem to solve
-     * @param var the variable to assign
-     * @param val the value of <code>var</code>
-     */
-    public void agressiveSolve( int level, SATProblem old_p, int var, boolean val )
-	throws SATResultException
-    {
-	if( traceSolver | traceNewCode ){
-	    System.err.println( "as" + level + ": Trying assignment var[" + var + "]=" + val );
-	}
-	SATProblem p = (SATProblem) old_p.clone();
-	p.propagateAssignment( var, val );
-	p.optimize();
-	if( traceSolver | traceNewCode ){
-	    p.report( System.err );
-	}
-	if( p.isConflicting() ){
-	    if( traceSolver | traceNewCode ){
-		System.err.println( "as" + level + ": found a conflict" );
-	    }
-	    return;
-	}
-	if( p.isSatisfied() ){
-	    SATSolution s = new SATSolution( p.buildInitialAssignments() );
-
-	    if( traceSolver | printSatSolutions ){
-		System.err.println( "as" + level + ": found a solution: " + s );
-	    }
-	    throw new SATResultException( s );
-	}
-
-	// Not yet solved, we'll have to do some work to solve it.
-
-	if( level<=firstVariables ){
-	    // Do another level of agressive solving.
-	    int nextvar = p.getMFUVariable();
-
-	    if( nextvar<0 ){
-		if( traceSolver | traceNewCode ){
-		    System.err.println( "as" + level + ": no variable to branch on, done." );
-		}
-		return;
-	    }
-	    agressiveSolve( level+1, p, nextvar, false );
-	    agressiveSolve( level+1, p, nextvar, true );
-	    sync();
-	}
-	else {
-	    // It's too expensive to do agressive solving, try something
-	    // a bit more subtle.
-	    SATContext ctx = new SATContext(
-	        p.getClauseCount(),
-		p.buildTermCounts(),
-		p.buildPosClauses(),
-		p.buildNegClauses()
-	    );
-
-	    ctx.varlist = p.buildOrderedVarList();
-
-	    if( ctx.varlist.length == 0 ){
-		// There are no variables left to assign, clearly there
-		// is no solution.
-		if( traceSolver | traceNewCode ){
-		    System.err.println( "as" + level + ": nothing to branch on" );
-		}
-		return;
-	    }
-
-	    ctx.assignments = p.buildInitialAssignments();
-
-	    if( leafVariables>=ctx.varlist.length ){
-		leafSolve( level+1, p, ctx.varlist, ctx.assignments, 0, false );
-		leafSolve( level+1, p, ctx.varlist, ctx.assignments, 0, true );
-	    }
 	    else {
-		SATContext negctx = (SATContext) ctx.clone();
-		int nextvar = ctx.getDecisionVariable();
-
 		solve( level+1, p, negctx, nextvar, false );
-		solve( level+1, p, ctx, nextvar, true );
-		sync();
+		solve( level+1, p, posctx, nextvar, true );
 	    }
+	    sync();
 	}
     }
 
@@ -298,44 +198,42 @@ public class SATSolver extends ibis.satin.SatinObject implements SATInterface, j
 	    if( var == -1 ){
 		return null;
 	    }
-	    if( firstVariables == 0 ){
-		// It's too expensive to do agressive solving, try something
-		// a bit more subtle.
-		SATContext ctx = new SATContext(
-		    p.getClauseCount(),
-		    p.buildTermCounts(),
-		    p.buildPosClauses(),
-		    p.buildNegClauses()
-		);
+	    SATContext ctx = new SATContext(
+		p.getClauseCount(),
+		p.buildTermCounts(),
+		p.buildPosClauses(),
+		p.buildNegClauses()
+	    );
 
-		ctx.varlist = p.buildOrderedVarList();
+	    ctx.varlist = p.buildOrderedVarList();
+	    ctx.assignments = p.buildInitialAssignments();
 
-		if( ctx.varlist.length == 0 ){
-		    // There are no variables left to assign, clearly there
-		    // is no solution.
-		    if( traceSolver | traceNewCode ){
-			System.err.println( "top: nothing to branch on" );
-		    }
-		    return null;
+	    int nextvar = ctx.getDecisionVariable();
+	    if( nextvar<0 ){
+		// There are no variables left to assign, clearly there
+		// is no solution.
+		if( traceSolver | traceNewCode ){
+		    System.err.println( "top: nothing to branch on" );
 		}
+		return null;
+	    }
 
-		ctx.assignments = p.buildInitialAssignments();
-
-		if( leafVariables>=ctx.varlist.length ){
-		    leafSolve( 0, p, ctx.varlist, ctx.assignments, 0, false );
-		    leafSolve( 0, p, ctx.varlist, ctx.assignments, 0, true );
-		}
-		else {
-		    SATContext negctx = (SATContext) ctx.clone();
-		    s.solve( 0, p, negctx, 0, false );
-		    s.solve( 0, p, ctx, 0, true );
-		}
+	    if( leafVariables>=ctx.varlist.length ){
+		leafSolve( 0, p, ctx.varlist, ctx.assignments, 0, false );
+		leafSolve( 0, p, ctx.varlist, ctx.assignments, 0, true );
 	    }
 	    else {
-		s.agressiveSolve( 0, p, var, false );
-		s.agressiveSolve( 0, p, var, true );
+		SATContext negctx = (SATContext) ctx.clone();
+		if( ctx.posDominant( nextvar ) ){
+		    s.solve( 0, p, ctx, nextvar, true );
+		    s.solve( 0, p, negctx, nextvar, false );
+		}
+		else {
+		    s.solve( 0, p, negctx, nextvar, false );
+		    s.solve( 0, p, ctx, nextvar, true );
+		}
+		s.sync();
 	    }
-	    s.sync();
 	}
 	catch( SATResultException r ){
 	    if( r.s == null ){
