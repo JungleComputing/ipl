5e604b2e06fcc5094481780c0a5c1161b2c3e357
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2004-03-02 13:13:23 +0000

    fixed more SOR stuf

--- apps/rmi/sor_grid/RemoteSORVisualizerInterface.java
+++
@@ -1,6 +0,0 @@
-interface RemoteSORVisualizerInterface extends java.rmi.Remote {
-	void putData(byte[][] data) throws java.rmi.RemoteException;
-
-	int width() throws java.rmi.RemoteException;
-	int height() throws java.rmi.RemoteException;
-}
