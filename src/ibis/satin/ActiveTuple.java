4f51a19c78b5b560242c17cd0bfa2705bf942687
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-03-02 09:17:59 +0000

    Added

---
+++ src/ibis/satin/ActiveTuple.java
@@ -0,0 +1,11 @@
+package ibis.satin;
+
+/** Active tuples should implement this interface.
+ When an active tuple is added to the tuple space (as data element),
+ the handleTuple method will be invoked on all machines, except the
+ one that inserted the element.  the inserted tuple key is passed as
+ parameter.
+**/
+interface ActiveTuple extends java.io.Serializable {
+	void handleTuple(String key);
+}
---
+++ src/ibis/satin/Tuple.java
@@ -0,0 +1,7 @@
+package ibis.satin;
+
+/** A tuple **/
+class Tuple implements java.io.Serializable {
+	String key;
+	java.io.Serializable data;
+}
