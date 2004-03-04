84f038675dca6aaa56488df2e6c5084d4a5b0773
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-03-04 09:23:54 +0000

    CvR: + Fixed a typo in the comments.

--- src/ibis/gmi/Personalizer.java
+++ src/ibis/gmi/Personalizer.java
@@ -2,16 +2,17 @@ package ibis.gmi;
 
 /**
  * The {@link Personalizer} class provides a base class for parameter
- * personalizers. A parameter personalizer is used to split parameter set of a
- * method invocation into several, personalized, sets of parameters.
+ * personalizers. A parameter personalizer is used to split the parameter set
+ * of a method invocation into several, personalized, sets of parameters.
  */
 public abstract class Personalizer { 
 
     /**
-     * Actually does the splitting.
+     * Splits the specified list of parameters, and writes them into
+     * the specified array of parameter lists.
      *
-     * @param in the input parameter set
-     * @param out the resulting personalized parameters
+     * @param in the input parameter list
+     * @param out the resulting personalized parameter lists
      */
     public abstract void personalize(ParameterVector in, ParameterVector [] out);
 } 
