072b008307549d5426aebcacbff303d4c05a277e
Maik Nijhuis <maik@cs.vu.nl>
2004-03-05 16:58:30 +0000

    The java files are active tuples for the tuple2 impl

---
+++ apps/satin/barnes_inline/TreeUpdater.java
@@ -0,0 +1,29 @@
+/*
+ * Active tuple: does the sequential work in the tuple2 version.
+ * A tuple contains the computed accelerations in the last force calculation
+ * phase
+ */
+
+final class TreeUpdater implements ibis.satin.ActiveTuple {
+    double[] accs_x;
+    double[] accs_y;
+    double[] accs_z;
+
+    TreeUpdater(double[] x, double[] y, double[] z) {
+	accs_x = x;
+	accs_y = y;
+	accs_z = z;
+    }
+
+    public void handleTuple(String key) {
+	System.out.println("Updating at iteration " + key);
+	int iteration = Integer.parseInt(key);
+
+	if (iteration > 0) {
+	    //the updates from the previous iteration have to be applied
+	    BarnesHut.updateBodies(accs_x, accs_y, accs_z, iteration - 1);
+	}
+
+	BarnesHut.buildTreeAndDoCoM();
+    }
+}
