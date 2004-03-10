9d84440b87b5550e1d50f4576189a48bf5ad4da8
Rutger Hofman <rutger@cs.vu.nl>
2004-03-10 19:55:31 +0000

    Add a TimeTimer

---
+++ apps/extra/time-timer/TimeTimer.java
@@ -0,0 +1,49 @@
+import ibis.util.Timer;
+
+/**
+ * Try to load a timer and measure its overhead
+ */
+class TimeTimer {
+
+    public static void main(String[] args) {
+
+	String timerName = "ibis.util.nativeCode.Rdtsc";
+	int options = 0;
+	int n = 1000000;
+
+	for (int i = 0; i < args.length; i++) {
+	    if (false) {
+	    } else if (options == 0) {
+		timerName = args[i];
+	    } else if (options == 1) {
+		n = Integer.parseInt(args[i]);
+	    } else {
+		System.err.println("Usage: TimeTimer {#calls {timer class}}");
+		System.exit(7);
+	    }
+	}
+
+	try {
+	    Timer timer = Timer.newTimer(timerName);
+	    Timer outerTimer = Timer.newTimer(timerName);
+
+	    long t = System.currentTimeMillis();
+	    outerTimer.start();
+	    for (int i = 0; i < n; i++) {
+		timer.start();
+		timer.stop();
+	    }
+	    outerTimer.stop();
+	    t = System.currentTimeMillis() - t;
+
+	    System.err.println("TimeTimer: " + n + " calls " +
+				" wallclock time " + (t / 1000.0) + "s " +
+				" own elapsed " +
+				(outerTimer.totalTimeVal() / 1000.0) + "s " +
+				" per tick " + (t * 1000.0 / n) + "us");
+	} catch (Exception e) {
+	    System.err.println("Got exception? " + e);
+	}
+
+    }
+}
