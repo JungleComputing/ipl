64bbca7b153fa7262ff205d43c86d11305510ddc
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-03-18 08:40:31 +0000

    CvR: + Added some javadoc comment.

--- src/ibis/satin/ActiveTuple.java
+++ src/ibis/satin/ActiveTuple.java
@@ -1,11 +1,17 @@
 package ibis.satin;
 
-/** Active tuples should implement this interface.
- When an active tuple is added to the tuple space (as data element),
- the handleTuple method will be invoked on all machines, including the
- one that inserted the element.  the inserted tuple key is passed as
- parameter.
+/**
+ * The marker interface of active tuples.
+ * Every active tuple should implement this interface.
+ * When an active tuple is added to the tuple space (as data element),
+ * the handleTuple method will be invoked on all machines, including the
+ * one that inserted the element. 
 **/
 public interface ActiveTuple extends java.io.Serializable {
+        /**
+         * Notifies that a new active tuple with the specified
+         * key has been added to the tuple space.
+         * @param key The key of the new active tuple.
+         */
 	public void handleTuple(String key);
 }
