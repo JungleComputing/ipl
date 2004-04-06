939aae0e308899d34d356fe53f63245b3685a16d
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-04-06 18:12:32 +0000

    Give nameserver time to startup

--- apps/satin/run.mk
+++ apps/satin/run.mk
@@ -68,12 +68,14 @@ test_java:
 
 test:
 	../../../bin/ibis_nameserver -single -port $(NAMESERVER_PORT) &
-	PRUN_ENV=test_one_pool ../../../bin/run_ibis 0 1 $(NAMESERVER_PORT) localhost $(MAIN_CLASS_NAME) $(TEST_APP_OPTIONS) -satin-stats -satin-closed > test_out 2> test_err
+	sleep 1
+	PRUN_ENV=test_one_pool ../../../bin/run_ibis 0 1 $(NAMESERVER_PORT) localhost $(MAIN_CLASS_NAME) $(TEST_APP_OPTIONS) -satin-tcp -satin-stats -satin-closed > test_out 2> test_err
 	grep "application result" test_out > test_res
 	diff test_res test_goal
 
 test_par:
 	../../../bin/ibis_nameserver -single -port $(NAMESERVER_PORT) &
+	sleep 2
 	rm -f test_par_out test_par_err test_par_res
 	PRUN_ENV=test_par_pool ../../../bin/run_ibis 0 2 $(NAMESERVER_PORT) localhost $(MAIN_CLASS_NAME) $(TEST_APP_OPTIONS) $(PAR_TEST_OPTIONS) -satin-stats -satin-closed >> test_par_out 2>> test_par_err &
 	PRUN_ENV=test_par_pool ../../../bin/run_ibis 1 2 $(NAMESERVER_PORT) localhost $(MAIN_CLASS_NAME) $(TEST_APP_OPTIONS) $(PAR_TEST_OPTIONS) -satin-stats -satin-closed >> test_par_out 2>> test_par_err
