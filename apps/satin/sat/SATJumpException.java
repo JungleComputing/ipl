e492c4a1e46f39f25f8ba15f10a175adff22032a
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-05-12 17:51:38 +0000

    CvR: + When renaming a class, also register the new class with CVS...

---
+++ apps/satin/sat/SATJumpException.java
@@ -0,0 +1,9 @@
+// File: $Id$
+
+/**
+ * The exception that is thrown when the SAT solver wants to do a restart.
+ */
+class SATJumpException extends SATException {
+    int level;
+    SATJumpException( int level ){ this.level = level; }
+}
