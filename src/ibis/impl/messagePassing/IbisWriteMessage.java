0a91a1f6a7e5fb01bcd12b5499cafdf90e31bb52
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-05-26 11:54:56 +0000

    Fix: only flush Ibis serialization stream when needed

--- src/ibis/impl/messagePassing/IbisWriteMessage.java
+++ src/ibis/impl/messagePassing/IbisWriteMessage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 final public class IbisWriteMessage extends WriteMessage {
 
     private ibis.io.IbisSerializationOutputStream obj_out;
+    private boolean needs_flush = true;
 
     IbisWriteMessage() {
     }
@@ -18,6 +19,7 @@ final public class IbisWriteMessage extends WriteMessage {
     public int send() throws IOException {
 // System.err.println("Send Ibis WriteMessage " + this + ": send its ByteOutput " + out + " flush its IbisSerializationOutputStream " + obj_out);
 	obj_out.flush();
+	needs_flush = false;
 	// out is flushed from obj_out.
 
 	return out.getSentFrags();
@@ -25,8 +27,11 @@ final public class IbisWriteMessage extends WriteMessage {
 
 
     public long finish() throws IOException {
-	obj_out.flush();
+	if (needs_flush) {
+	    obj_out.flush();
+	}
 	obj_out.reset();
+	needs_flush = true;
 	return super.finish();
     }
 
@@ -35,124 +40,152 @@ final public class IbisWriteMessage extends WriteMessage {
     }
 
     public void writeBoolean(boolean value) throws IOException {
+	needs_flush = true;
 	obj_out.writeBoolean(value);
     }
 
     public void writeByte(byte value) throws IOException {
+	needs_flush = true;
 	obj_out.writeByte(value);
     }
 
     public void writeChar(char value) throws IOException {
+	needs_flush = true;
 	obj_out.writeChar(value);
     }
 
     public void writeShort(short value) throws IOException {
+	needs_flush = true;
 	obj_out.writeShort(value);
     }
 
     public void writeInt(int value) throws IOException {
+	needs_flush = true;
 	obj_out.writeInt(value);
     }
 
     public void writeLong(long value) throws IOException {
+	needs_flush = true;
 	obj_out.writeLong(value);
     }
 
     public void writeFloat(float value) throws IOException {
+	needs_flush = true;
 	obj_out.writeFloat(value);
     }
 
     public void writeDouble(double value) throws IOException {
+	needs_flush = true;
 	obj_out.writeDouble(value);
     }
 
     public void writeObject(Object value) throws IOException {
+	needs_flush = true;
 	obj_out.writeObject(value);
     }
 
     public void writeString(String value) throws IOException { 
+	needs_flush = true;
 	writeObject(value);
     }
 
     public void writeArray(boolean[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(byte[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(char[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(short[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(int[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(long[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(float[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(double[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
     public void writeArray(Object[] value, int offset, int size)
 	    throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value, offset, size);
     }
 
 
     public void writeArray(boolean[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(byte[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(char[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(short[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(int[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(long[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(float[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(double[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 
     public void writeArray(Object[] value) throws IOException {
+	needs_flush = true;
 	obj_out.writeArray(value);
     }
 }
