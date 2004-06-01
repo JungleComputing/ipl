aed07f46266b691d5e3bc7c50ed1336c2ab2424f
Gosia Wrezinska <gosia@cs.vu.nl>
2004-06-01 14:50:24 +0000

    *** empty log message ***

---
+++ src/ibis/satin/DeleteThread.java
@@ -0,0 +1,26 @@
+package ibis.satin;
+
+import ibis.ipl.Ibis;
+
+class DeleteThread extends Thread {
+
+Satin satin;
+int milis;
+
+DeleteThread(Satin satin, int time) {
+    
+    super("SatinDeleteThread");
+    this.satin = satin;
+    this.milis = 1000 * time;
+}
+
+public void run() {
+    try {
+	sleep(milis);
+    } catch (InterruptedException e) {
+	//ignore
+    }
+    satin.delete(satin.ident);
+}
+
+}
---
+++ src/ibis/satin/KillerThread.java
@@ -0,0 +1,22 @@
+package ibis.satin;
+
+class KillerThread extends Thread {
+
+int milis;
+
+KillerThread(int time) {
+    super("SatinKillerThread");
+    milis = time * 1000;
+}
+
+public void run() {
+    try {
+	sleep(milis);
+    } catch (InterruptedException e) {
+	//ignore
+    }
+    System.exit(1);
+}
+
+}
+    
