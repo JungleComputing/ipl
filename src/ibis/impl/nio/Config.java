94992c98dba620cee9bb720a5c8d6826665b3f14
Niels Drost <n.drost@esciencecenter.nl>
2004-06-23 12:04:31 +0000

    disabled debug support

--- src/ibis/impl/nio/Config.java
+++ src/ibis/impl/nio/Config.java
@@ -9,7 +9,7 @@ interface Config {
      * Enables debug support in nio ibis. Use the ibis.nio.debug system
      * property to select which debug statements to print. See Debug.java
      */
-    static final boolean DEBUG = true;
+    static final boolean DEBUG = false;
 
     /*
      * Do we do asserts (may make it a bit slower)
