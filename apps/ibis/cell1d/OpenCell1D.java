874df9630d1765c650a72965b4b36d7e4b1e48e9
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-06-25 13:53:50 +0000

    CvR: + Make sure we don't send all our columns (or even more) to our right neighbour.

--- apps/ibis/cell1d/OpenCell1D.java
+++ apps/ibis/cell1d/OpenCell1D.java
@@ -379,7 +379,16 @@ class OpenCell1D implements OpenConfig {
         }
         int sendCount;
         if( p.firstColumn<boardsize ){
-            sendCount = p.firstNoColumn-aimFirstNoColumn;
+            int currentAimFirstNoColumn = aimFirstNoColumn;
+
+            // Make sure we have at least two columns left.
+            if( currentAimFirstNoColumn<p.firstColumn+2 ){
+                currentAimFirstNoColumn = p.firstColumn+2;
+                if( traceLoadBalancing ){
+                    System.out.println( "P" + me + ":" + generation + ": P" + (me+1) + " cannot get all my columns, I keep 2" );
+                }
+            }
+            sendCount = p.firstNoColumn-currentAimFirstNoColumn;
             if( sendCount<0 ){
                 sendCount = 0;
             }
