7d8d4ccc3421e34a782b8959c17651df93f695a7
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-07-01 09:29:46 +0000

    CvR: + Fixed a typo in a comment.

--- src/ibis/satin/Inlet.java
+++ src/ibis/satin/Inlet.java
@@ -2,7 +2,7 @@ package ibis.satin;
 
 /**
  * Optional class that application Exceptions (inlets) can extend. The advantage
- * of this is that the generation of stack traces (an expensive operation is
+ * of this is that the generation of stack traces (an expensive operation) is
  * inhibited.
  */
 public class Inlet extends Throwable {
