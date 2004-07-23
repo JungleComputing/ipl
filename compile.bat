2a97937f520ad687d0ee30bfe2c492e802f3e502
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-07-23 21:36:34 +0000

    Additions

---
+++ compile.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if NOT DEFINED JAVA_HOME goto javahome
+if "%JAVA_HOME%" == "" goto javahome
+
+3rdparty\apache-ant-1.6.1\bin\ant %1 %2 %3 %4 %5
+goto end
+
+:javahome
+echo The enviroment variable JAVA_HOME must be set to the current jdk 
+echo distribution installed on your computer, and must be a 1.4 version
+echo or newer.
+goto end
+
+:end
