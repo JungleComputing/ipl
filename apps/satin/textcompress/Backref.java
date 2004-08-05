4a144f6f97a55c3c578311e224cd538a08aeb813
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-08-05 09:38:20 +0000

    CvR: + Added a serializable to make the satin compiler happy.

--- apps/satin/textcompress/Backref.java
+++ apps/satin/textcompress/Backref.java
@@ -1,6 +1,6 @@
 // File: $Id$
 
-class Backref {
+class Backref implements java.io.Serializable {
     int backpos = -1;
     int len = -1;
     int gain = 0;       // The gain of using this backref.
