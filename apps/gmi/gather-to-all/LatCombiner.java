66c18b721fb166312d5fd4186a6e204aaa7f759d
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-09-17 10:36:53 +0000

    obsoleted by invocation-gather

--- apps/gmi/gather-to-all/LatCombiner.java
+++
@@ -1,9 +0,0 @@
-import ibis.gmi.*;
-
-//public class LatCombiner extends FlatInvocationCombiner { 
-//    public void combine(ParameterVector [] in, ParameterVector out) {} 
-//} 
-//
-public class LatCombiner extends BinomialInvocationCombiner {
-    public void combine(ParameterVector in1, ParameterVector in2, ParameterVector out) { }
-}
--- apps/gmi/gather/LatCombiner.java
+++
@@ -1,5 +0,0 @@
-import ibis.gmi.*;
-
-public class LatCombiner extends FlatInvocationCombiner { 
-    public void combine(ParameterVector [] in, ParameterVector out) {} 
-} 
