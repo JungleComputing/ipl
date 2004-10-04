a219911b3c840cf3e44b299eb0dc66cf3ce04176
Niels Drost <n.drost@esciencecenter.nl>
2004-10-04 07:52:55 +0000

    caculate throughput in MiB/sec (not MB/s) make nr of warmups flexible

--- apps/io/benchmarks/conversion/Main.java
+++ apps/io/benchmarks/conversion/Main.java
@@ -6,6 +6,11 @@ public final class Main {
 
     Random random = new Random();
 
+    static final int MIN_BUF_SIZE = 100 * 1024;
+    static final int MAX_BUF_SIZE = 100 * 1024;
+    static final int TOTAL_TEST_DATA_SIZE = 1024 * 1024 * 1024;
+    static final int REPEATS = 5;
+
     /**
      * Measure conversion speed for various primitive arrays, depending on
      * array size.
@@ -25,17 +30,17 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 
 	    // *** booleans ***
 
 	    boolean[] booleans = new boolean[bufferLength / Conversion.BOOLEAN_SIZE];
-	    int ITERS = 16 * (1 << 20) / (booleans.length * Conversion.BOOLEAN_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (booleans.length * Conversion.BOOLEAN_SIZE);
 	    byte[] buf = new byte[booleans.length * Conversion.BOOLEAN_SIZE];
 	    double thrp;
 	    double MBs = ITERS * booleans.length * (double)Conversion.BOOLEAN_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -43,10 +48,10 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("boolean2byte[" + booleans.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -54,7 +59,7 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2boolean[" + booleans.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
 	}
@@ -66,16 +71,16 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 	    // *** shorts ***
 
 	    short[] shorts = new short[bufferLength / Conversion.SHORT_SIZE];
-	    int ITERS = 16 * (1 << 20) / (shorts.length * Conversion.SHORT_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (shorts.length * Conversion.SHORT_SIZE);
 	    byte[] buf = new byte[shorts.length * Conversion.SHORT_SIZE];
 	    double thrp;
 	    double MBs = ITERS * shorts.length * (double)Conversion.SHORT_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -83,10 +88,10 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("short2byte[" + shorts.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -94,7 +99,7 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2short[" + shorts.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
 	}
@@ -105,38 +110,36 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 
 	    // *** ints ***
 
 	    int[] ints = new int[bufferLength / Conversion.INT_SIZE];
-	    int ITERS = 16 * (1 << 20) / (ints.length * Conversion.INT_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (ints.length * Conversion.INT_SIZE);
 	    byte[] buf = new byte[ints.length * Conversion.INT_SIZE];
 	    double thrp;
 	    double MBs = ITERS * ints.length * (double)Conversion.INT_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.int2byte(ints, 0, ints.length, buf, 0);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("int2byte[" + ints.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
-
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    }
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.byte2int(buf, 0, ints, 0, ints.length);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2int[" + ints.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
-
+	    }
 	}
 
     }
@@ -146,37 +149,37 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 
 	    // *** longs ***
 
 	    long[] longs = new long[bufferLength / Conversion.LONG_SIZE];
-	    int ITERS = 16 * (1 << 20) / (longs.length * Conversion.LONG_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (longs.length * Conversion.LONG_SIZE);
 	    byte[] buf = new byte[longs.length * Conversion.LONG_SIZE];
 	    double thrp;
 	    double MBs = ITERS * longs.length * (double)Conversion.LONG_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.long2byte(longs, 0, longs.length, buf, 0);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("long2byte[" + longs.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
+	    }
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.byte2long(buf, 0, longs, 0, longs.length);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2long[" + longs.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
+	    }
 
 	}
 
@@ -187,17 +190,17 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 
 	    // *** floats ***
 
 	    float[] floats = new float[bufferLength / Conversion.FLOAT_SIZE];
-	    int ITERS = 16 * (1 << 20) / (floats.length * Conversion.FLOAT_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (floats.length * Conversion.FLOAT_SIZE);
 	    byte[] buf = new byte[floats.length * Conversion.FLOAT_SIZE];
 	    double thrp;
 	    double MBs = ITERS * floats.length * (double)Conversion.FLOAT_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -205,10 +208,10 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("float2byte[" + floats.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
@@ -216,7 +219,7 @@ public final class Main {
 		}
 		timer.stop();
 	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2float[" + floats.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
 
 	}
@@ -228,38 +231,36 @@ public final class Main {
 
 	ibis.util.nativeCode.Rdtsc timer = new ibis.util.nativeCode.Rdtsc();
 
-	for(int bufferLength = 64; bufferLength < (2 << 20); bufferLength *= 2) {
+	for(int bufferLength = MIN_BUF_SIZE; bufferLength <= MAX_BUF_SIZE; bufferLength *= 2) {
 
 	    // *** doubles ***
 
 	    double[] doubles = new double[bufferLength / Conversion.DOUBLE_SIZE];
-	    int ITERS = 16 * (1 << 20) / (doubles.length * Conversion.DOUBLE_SIZE);
+	    int ITERS = TOTAL_TEST_DATA_SIZE / (doubles.length * Conversion.DOUBLE_SIZE);
 	    byte[] buf = new byte[doubles.length * Conversion.DOUBLE_SIZE];
 	    double thrp;
 	    double MBs = ITERS * doubles.length * (double)Conversion.DOUBLE_SIZE / (1 << 20);
 
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.double2byte(doubles, 0, doubles.length, buf, 0);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("double2byte[" + doubles.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
-
-	    for (int warmup = 0; warmup < 2; warmup ++) {
+	    }
+	    for (int warmup = 0; warmup < REPEATS; warmup ++) {
 		timer.reset();
 		timer.start();
 		for (int i = 0; i < ITERS; i++) {
 		    conversion.byte2double(buf, 0, doubles, 0, doubles.length);
 		}
 		timer.stop();
-	    }
-	    thrp = MBs / (timer.totalTimeVal() / 1000000.0);
+	    thrp = MBs / (timer.totalTimeVal() / 1048576.0);
 	    System.out.println("byte2double[" + doubles.length + "] (" + ITERS + ") takes " + doubleFmt((timer.totalTimeVal() / ITERS), 1) + " us; thrp " + doubleFmt(thrp, 1) + " MB/s");
-
+	    }
 	}
 
     }
@@ -278,8 +279,8 @@ public final class Main {
 	    conversion = Conversion.loadConversion(args[0]);
 
 	    System.out.println("Now look into " + conversion);
+	    intConversion(conversion);
 	    longConversion(conversion);
-	    floatConversion(conversion);
 	    doubleConversion(conversion);
 
 	} catch (Exception e) {
