437b27a7620c76b7f4317a2422ce5e96864fe7b9
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-10-05 08:28:29 +0000

    Set ANT_HOME in compile.bat

--- compile.bat
+++ compile.bat
@@ -3,6 +3,14 @@
 if NOT DEFINED JAVA_HOME goto javahome
 if "%JAVA_HOME%" == "" goto javahome
 
+if defined ANT_HOME (
+        echo You have the ANT_HOME environment variable set.
+	echo I am resetting it to 3rdparty\apache-ant-1.6.1
+
+)
+
+set ANT_HOME=3rdparty\apache-ant-1.6.1
+
 3rdparty\apache-ant-1.6.1\bin\ant %1 %2 %3 %4 %5
 goto end
 
