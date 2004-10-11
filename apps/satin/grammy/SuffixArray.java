2d7c7eb32840ceceb36362221d53aaf8df4764db
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-10-11 14:25:56 +0000

    CvR: + New longest string repeat algorithm implemented but disabled.

--- apps/satin/grammy/SuffixArray.java
+++ apps/satin/grammy/SuffixArray.java
@@ -123,6 +123,156 @@ public class SuffixArray implements Configuration, Magic, java.io.Serializable {
 	return isSmallerCharacter( i0+n, i1+n );
     }
 
+
+    /** Fills the given index and commonality arrays with groups
+     * of elements sorted according to the character at the given offset.
+     */
+    private int sort1(
+        int next[],
+        int indices1[],
+        boolean comm[],
+        int p,
+        int indices[],
+        int start,
+        int end,
+        int offset
+    )
+    {
+        int slot[] = new int[nextcode];
+        int prev[] = new int[nextcode];
+
+        java.util.Arrays.fill( prev, -1 );
+        java.util.Arrays.fill( slot, -1 );
+
+        // Fill the hash buckets
+        for( int n=start; n<end; n++ ){
+            int i = indices[n];
+
+            if( i+offset<length ){
+                short ix = text[i+offset];
+
+                if( prev[ix] == -1 ){
+                    slot[ix] = i;
+                }
+                else {
+                    next[prev[ix]] = i;
+                }
+                prev[ix] = i;
+            }
+        }
+
+        // Terminate all lists.
+        for( int n=0; n<nextcode; n++ ){
+            int i = prev[n];
+
+            if( i != -1 ){
+                next[i] = -1;
+            }
+        }
+
+        // Write out the indices and commonalities.
+        for( int n=0; n<nextcode; n++ ){
+            int i = slot[n];
+            boolean c = false;
+
+            if( i != -1 && next[i] != -1 ){
+                // There is a repeat, write out this chain.
+                while( i != -1 ){
+                    indices1[p] = i;
+                    comm[p] = c;
+                    i = next[i];
+                    c = true;
+                    p++;
+                }
+            }
+        }
+        return p;
+    }
+
+    /** Determine whether the given list of indices is useful for the
+        non-overlapping repeat we're searching. Too short or only overlapping
+        repeats cause a reject.
+    */
+    private boolean isAcceptable( int indices[], int start, int end, int offset )
+    {
+        if( end-start<2 ){
+            return false;
+        }
+        for( int ix=start; ix<end; ix++ ){
+            for( int iy=ix+1; ix<end; ix++ ){
+                if( (indices[ix]+offset)<=indices[iy] ){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    class Result {
+        int offset;
+        int l;
+        int indices[];
+        boolean comm[];
+
+        Result( int offset, int l, int indices[], boolean comm[] )
+        {
+            this.offset = offset;
+            this.l = l;
+            this.indices = indices;
+            this.comm = comm;
+        }
+    }
+
+    /** Returns a selection of the suffix array that allows easy extraction
+     * of the longest repeats.
+     */
+    private Result selectRepeats()
+    {
+        int next[] = new int[length];
+        int indices[] = new int[length];
+        boolean comm[] = new boolean[length];
+        int offset = 0;
+        int indices1[] = new int[length];
+        boolean comm1[] = new boolean[length];
+
+        for( int i=0; i<length; i++ ){
+            indices[i] = i;
+        }
+        int l = sort1( next, indices, comm, 0, indices, 0, length, offset );
+        for(;;){
+            int ix = 0;
+            offset++;
+            boolean acceptable = false;
+            int p = 0;
+            while( ix<l ){
+                int start = ix;
+                int oldp = p;
+
+                ix++;
+                while( ix<length && comm[ix] ){
+                    ix++;
+                }
+                p = sort1( next, indices1, comm1, p, indices, start, ix, offset );
+                if( isAcceptable( indices1, oldp, p, offset ) ){
+                    acceptable = true;
+                    comm1[oldp] = false;
+                }
+            }
+            if( !acceptable ){
+                break;
+            }
+            // Swap the scratch version and the accepted version.
+            int h[] = indices;
+            indices = indices1;
+            indices = h;
+            boolean hc[] = comm;
+            comm1 = comm;
+            comm = hc;
+            l = p;
+        }
+        return new Result( offset-1, l, indices, comm );
+    }
+
     /** Sorts the given range. */
     private void sort( int indices[], int commonality[], int start, int end )
     {
@@ -457,64 +607,95 @@ public class SuffixArray implements Configuration, Magic, java.io.Serializable {
      */
     public StepList selectBestSteps( int top )
     {
-        int mincom = MINCOMMONALITY;
-        int candidates[] = new int[length];
-        int p = 0;
+        StepList res;
 
-	/** The indices in increasing alphabetical order of their suffix. */
-	int indices[] = new int[length];
+        if( true ){
+            int p = 0;
+            int candidates[] = new int[length];
+            int mincom = MINCOMMONALITY;
 
-	/** For each position in `indices', the number of elements it
-	 * has in common with the previous entry, or -1 for element 0.
-	 * The commonality may be overlapping, and that should be taken
-	 * into account by compression algorithms.
-	 */
-	int commonality[] = new int[length];
+            /** The indices in increasing alphabetical order of their suffix. */
+            int indices[] = new int[length];
 
-        buildAdministration( indices, commonality );
+            /** For each position in `indices', the number of elements it
+             * has in common with the previous entry, or -1 for element 0.
+             * The commonality may be overlapping, and that should be taken
+             * into account by compression algorithms.
+             */
+            int commonality[] = new int[length];
 
+            buildAdministration( indices, commonality );
+            res = new StepList( top );
+            for( int i=1; i<length; i++ ){
+                if( commonality[i]>=mincom ){
+                    // A new candidate match. Start a list, and see if we
+                    // get at least two non-overlapping strings.
+                    int pos0 = indices[i-1];
+                    candidates[0] = pos0;
+                    p = 1;
 
-        StepList res = new StepList( top );
-        for( int i=1; i<length; i++ ){
-            if( commonality[i]>=mincom ){
-                // A new candidate match. Start a list, and see if we
-                // get at least two non-overlapping strings.
-                int pos0 = indices[i-1];
-                candidates[0] = pos0;
-                p = 1;
+                    int len = commonality[i];
 
-                int len = commonality[i];
+                    while( len>mincom ){
+                        // Now search for non-overlapping substrings that 
+                        // are equal for `len' characters. All possibilities
+                        // are the subsequent entries in the suffix array, up to
+                        // the first one with less than 'len' characters
+                        // commonality.
+                        // We must test each one for overlap with all
+                        // previously selected strings.
+                        // TODO: this fairly arbitrary way of gathering candidates
+                        // may not be optimal: a different subset of strings may
+                        // be larger.
+                        int j = i;
+                        while( j<length && commonality[j]>=len ){
+                            int posj = indices[j];
 
-                while( len>mincom ){
-		    // Now search for non-overlapping substrings that 
-		    // are equal for `len' characters. All possibilities
-		    // are the subsequent entries in the suffix array, up to
-                    // the first one with less than 'len' characters
-                    // commonality.
-		    // We must test each one for overlap with all
-		    // previously selected strings.
-                    // TODO: this fairly arbitrary way of gathering candidates
-                    // may not be optimal: a different subset of strings may
-                    // be larger.
-		    int j = i;
-		    while( j<length && commonality[j]>=len ){
-			int posj = indices[j];
+                            if( !areOverlapping( candidates, p, posj, len ) ){
+                                candidates[p++] = posj;
+                            }
+                            j++;
+                        }
 
-			if( !areOverlapping( candidates, p, posj, len ) ){
-			    candidates[p++] = posj;
-			}
-			j++;
-		    }
-
-                    if( p>1 ){
-                        // HEURISTIC: anything shorter than this is probably
-                        // not a reasonable candidate for best compression step.
-                        // (But we could be wrong.)
-                        // mincom = Math.max( mincom, len-1 );
-                        res.add( new Step( candidates, p, len ) );
+                        if( p>1 ){
+                            // HEURISTIC: anything shorter than this is probably
+                            // not a reasonable candidate for best compression step.
+                            // (But we could be wrong.)
+                            // mincom = Math.max( mincom, len-1 );
+                            res.add( new Step( candidates, p, len ) );
+                        }
+                        len--;
                     }
-                    len--;
-		}
+                }
+            }
+        }
+        else {
+            Result reps = selectRepeats();
+            boolean commonality[] = reps.comm;
+            int indices[] = reps.indices;
+            int l = reps.l;
+            int offset = reps.offset;
+            int candidates[] = new int[l];
+
+            res = new StepList( top );
+            int ix = 0;
+            while( ix<l ){
+                int start = ix;
+                candidates[0] = indices[ix];
+                int p = 1;
+
+                ix++;
+                while( ix<l && commonality[ix] ){
+                    int posj = indices[ix];
+
+                    if( !areOverlapping( candidates, p, posj, offset ) ){
+                        candidates[p++] = posj;
+                    }
+                    ix++;
+                }
+                if( p>1 ){
+                    res.add( new Step( candidates, p, offset ) );
+                }
             }
         }
         return res;
