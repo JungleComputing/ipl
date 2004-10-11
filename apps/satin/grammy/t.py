692427f7cae22b51d3a54fce8819ee0ef43e49c9
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-10-11 11:25:09 +0000

    CvR: + New version of the algorithm produces less garbage.

--- apps/satin/grammy/t.py
+++ apps/satin/grammy/t.py
@@ -1,4 +1,4 @@
-def sort1( text, next, indices, offset ):
+def sort1( text, next, ix1, comm, p, indices, offset ):
     """ Returns an index and commonality array sorted by the character
     at the given offset."""
     slot = 256*[-1]
@@ -16,58 +16,61 @@ def sort1( text, next, indices, offset ):
     for i in prev:
         if i != -1:
             next[i] = -1
-    res = []
-    comm = []
     for i in slot:
         j = i
         c = 0
         if j != -1 and next[j] != -1:
             while j != -1:
-                res.append( j )
-                comm.append( c )
+                ix1[p] = j
+                comm[p] = c
                 j = next[j]
                 c = 1
-    return (res, comm)
+                p += 1
+    return p
 
-def isAcceptable( indices, offset ):
-    """ Determine whether the given range of indices is useful for the
+def isAcceptable( indices, start, end, offset ):
+    """ Determine whether the given list of indices is useful for the
     non-overlapping repeat we're searching. Too short or only overlapping
     repeats cause a reject. """
-    if len( indices )<2:
+    if end-start<2:
         return 0
-    for ix in range( len(indices) ):
-        for iy in range( ix+1, len( indices ) ):
+    for ix in range( start, end ):
+        for iy in range( ix+1, end ):
             if (indices[ix]+offset)<=indices[iy]:
                 return 1
     return 0
 
 def sort( text ):
     """ Returns the fully sorted text."""
-    next = len( text )*[0]
+    sz = len( text )
+    next = sz*[0]
+    indices = sz*[0]
+    comm = sz*[0]
     offset = 0
-    (indices,comm) = sort1( text, next, range( len( text ) ), offset )
+    l = sort1( text, next, indices, comm, 0, range( len( text ) ), offset )
     while 1:
-        indices1 = []
-        comm1 = []
+        indices1 = sz*[0]
+        comm1 = sz*[0]
         ix = 0
         offset += 1
         acceptable = 0
+        p = 0
         while ix<len( indices ):
             start = ix
+            oldp = p
             ix = ix+1
             while ix<len( indices ) and comm[ix] != 0:
                 ix = ix+1
-            (i1,c1) = sort1( text, next, indices[start:ix], offset )
-            if isAcceptable( i1, offset ):
+            p = sort1( text, next, indices1, comm1, p, indices[start:ix], offset )
+            if isAcceptable( indices1, oldp, p, offset ):
                 acceptable = 1
-                c1[0] = 0
-                indices1 += i1
-                comm1 += c1
+                comm1[oldp] = 0
         if acceptable == 0:
             break
         indices = indices1
         comm = comm1
-    return( offset-1, indices, comm )
+        l = p
+    return( offset-1, l, indices, comm )
 
 def sort2( text ):
     l = range( len( text ) )
@@ -76,8 +79,8 @@ def sort2( text ):
 text = [0, 1, 0, 1, 2, 3, 3, 4, 2, 1, 0, 1, 2, 3, 3, 4, 2, 1, 0, 1, 2, 2, 3,3,3]
 #text = 10*[0, 1, 2] + [0,1]
 #(ix,comm) = sort2( text )
-(offset,ix,comm) = sort( text )
+(offset,l,ix,comm) = sort( text )
 
-print "offset:", offset, "commonality:", comm
-for i in range( len( ix ) ):
+print "offset:", offset, "commonality:", comm[0:l]
+for i in range( l ):
     print "[%2d] %2d: %s" % (comm[i], ix[i], text[ix[i]:])
