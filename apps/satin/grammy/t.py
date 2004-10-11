f55732c499df6e84e5e9c4e12426bcf402156d05
Kees van Reeuwijk <keesvr@xs4all.nl>
2004-10-11 08:43:20 +0000

    CvR: + Added code to keep searching for the longest commonality.

--- apps/satin/grammy/t.py
+++ apps/satin/grammy/t.py
@@ -31,27 +31,34 @@ def sort1( text, indices, offset ):
 def sort( text ):
     """ Returns the fully sorted text."""
     (indices,comm) = sort1( text, range( len( text ) ), 0 )
-    indices1 = []
-    comm1 = []
-    ix = 0
-    while ix<len( indices ):
-        start = ix
-        ix = ix+1
-        while ix<len( indices ) and comm[ix] == 1:
+    offset = 0
+    while 1:
+        indices1 = []
+        comm1 = []
+        ix = 0
+        offset += 1
+        while ix<len( indices ):
+            start = ix
             ix = ix+1
-        print "range: %d:%d" % ( start, ix )
-        (i1,c1) = sort1( text, indices[start:ix], 1 )
-        if len( c1 ) != 0:
-            c1[0] = comm[start]
-        indices1 += i1
-        comm1 += c1
-    return( indices1, comm1 )
+            while ix<len( indices ) and comm[ix] == offset:
+                ix = ix+1
+            (i1,c1) = sort1( text, indices[start:ix], offset )
+            if len( c1 ) != 0:
+                c1[0] = comm[start]
+            indices1 += i1
+            comm1 += c1
+        if len( indices1 ) <2:
+            break
+        indices = indices1
+        comm = comm1
+    return( indices, comm )
 
 def sort2( text ):
     l = range( len( text ) )
     return sort1( text, l, 0 )
 
-text = [0, 1, 0, 1, 2, 3, 3, 4, 2, 1, 0, 1, 2]
+#text = [0, 1, 0, 1, 2, 3, 3, 4, 2, 1, 0, 1, 2, 3, 3, 4, 2, 1, 0, 1, 2]
+text = 10*[0, 1, 2]
 #(ix,comm) = sort2( text )
 (ix,comm) = sort( text )
 
