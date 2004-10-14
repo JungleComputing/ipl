c74a49f3033594d42990a40bb965f6b2f6d31bf4
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2004-10-14 17:46:29 +0000

    Changed installation again: re-introduced and improved "ant" script

--- 3rdparty/apache-ant-1.6.1/bin/ant.bat
+++ 3rdparty/apache-ant-1.6.1/bin/ant.bat
@@ -21,7 +21,8 @@ if "%OS%"=="Windows_NT" @setlocal
 rem %~dp0 is expanded pathname of the current script under NT
 set DEFAULT_ANT_HOME=%~dp0..
 
-if "%ANT_HOME%"=="" set ANT_HOME=%DEFAULT_ANT_HOME%
+rem if "%ANT_HOME%"=="" set ANT_HOME=%DEFAULT_ANT_HOME%
+set ANT_HOME=%~dp0..
 set DEFAULT_ANT_HOME=
 
 rem Slurp the command line arguments. This loop allows for an unlimited number
