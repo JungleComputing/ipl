2d522f5f2af70638a0f4e0dc4c213d2b4b65ba2e
Rutger Hofman <rutger@cs.vu.nl>
2004-10-20 21:04:36 +0000

    Move RMI_init to directory shared/ here. Add IPSocketFactory for RMISocketFactory usage

---
+++ apps/rmi/shared/IPMapSocketFactory.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.InetAddress;
+
+import java.rmi.server.RMISocketFactory;
+
+import ibis.util.IPUtils;
+
+public class IPMapSocketFactory extends RMISocketFactory {
+
+    private final static boolean DEBUG = true;
+
+    private InetAddress myAddr = IPUtils.getLocalHostAddress();
+
+    public IPMapSocketFactory() {
+	if (DEBUG) {
+	    System.err.println("My local hostaddr " + myAddr);
+	}
+    }
+
+    public ServerSocket createServerSocket(int port) throws IOException {
+	ServerSocket s = new ServerSocket(port, 0, myAddr);
+	if (DEBUG) {
+	    System.err.println("Created new ServerSocket " + s);
+	}
+	return s;
+    }
+
+    public Socket createSocket(String host, int port) throws IOException {
+	Socket s = null;
+	try {
+	    s = new Socket(host, port, myAddr, 0);
+	} catch (IOException e) {
+	    s = new Socket(host, port);
+	}
+	if (DEBUG) {
+	    System.err.println("Created new Socket " + s);
+	}
+	return s;
+    }
+
+}
