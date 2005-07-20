903a6578e9f4d93fbea6923e50ac981e54272694
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2005-07-20 09:18:39 +0000

    *** empty log message ***

--- ant-tasks/HostName.java
+++ ant-tasks/HostName.java
@@ -11,90 +11,90 @@ public class HostName extends Task {
 
     private final static boolean DEBUG = false;
 
-    private String hostname;		// What is this used for?
-    private String domainname;		// What is this used for?
+    private String hostname; // What is this used for?
 
+    private String domainname; // What is this used for?
 
     private String domainName(String hostName) {
-	int dot = hostName.indexOf('.');
-	if (dot == -1) {
-	    return null;
-	}
+        int dot = hostName.indexOf('.');
+        if (dot == -1) {
+            return null;
+        }
 
-	return hostName.substring(dot + 1);
+        return hostName.substring(dot + 1);
     }
 
     public void execute() throws BuildException {
-	String hostName = null;
-	String domainName = null;
-	InetAddress addr = null;
+        String hostName = null;
+        String domainName = null;
+        InetAddress addr = null;
 
-	try {
-	    addr = InetAddress.getLocalHost();
-	    if (DEBUG) {
-		System.err.println("getLocalHost -> " + addr);
-	    }
-	    InetAddress[] nameAddr = InetAddress.getAllByName(addr.getHostAddress());
+        try {
+            addr = InetAddress.getLocalHost();
+            if (DEBUG) {
+                System.err.println("getLocalHost -> " + addr);
+            }
+            InetAddress[] nameAddr = InetAddress.getAllByName(addr
+                    .getHostAddress());
 
-	    for (int i = 0; i < nameAddr.length; i++) {
-		if (DEBUG) {
-		    System.err.println("getByName(getHostaddress)[" + i
-					+ "] -> " + nameAddr[i]);
-		}
-		hostName = nameAddr[i].getHostName();
-		if (DEBUG) {
-		    System.err.println("getHostName(--) -> " + hostName);
-		}
-		domainName = domainName(hostName);
-		if (domainName != null) {
-		    break;
-		}
-	    }
+            for (int i = 0; i < nameAddr.length; i++) {
+                if (DEBUG) {
+                    System.err.println("getByName(getHostaddress)[" + i
+                            + "] -> " + nameAddr[i]);
+                }
+                hostName = nameAddr[i].getHostName();
+                if (DEBUG) {
+                    System.err.println("getHostName(--) -> " + hostName);
+                }
+                domainName = domainName(hostName);
+                if (domainName != null) {
+                    break;
+                }
+            }
 
-	} catch (Exception e) {
-	    throw new BuildException("Could not find my host name" + e);
-	}
+        } catch (Exception e) {
+            throw new BuildException("Could not find my host name" + e);
+        }
 
-	if (domainName == null && addr != null) {
-	    /* It seems our reverse lookup has failed miserably.
-	     * Maybe the direct lookup can help a bit... */
-	    String unchecked = addr.toString();
-	    int slash = unchecked.indexOf('/');
-	    if (slash > 0) {
-		hostName = unchecked.substring(0, slash);
-		domainName = domainName(hostName);
-	    }
-	}
+        if (domainName == null && addr != null) {
+            /* It seems our reverse lookup has failed miserably.
+             * Maybe the direct lookup can help a bit... */
+            String unchecked = addr.toString();
+            int slash = unchecked.indexOf('/');
+            if (slash > 0) {
+                hostName = unchecked.substring(0, slash);
+                domainName = domainName(hostName);
+            }
+        }
 
-	if (hostName == null) {
-	    hostName = "(localhost)";
-	}
-	if (domainName == null) {
-	    domainName = "(none)";
-	}
+        if (hostName == null) {
+            hostName = "(localhost)";
+        }
+        if (domainName == null) {
+            domainName = "(none)";
+        }
 
-	addProperty("hostname", hostName);
-	addProperty("domainname", domainName);
+        addProperty("hostname", hostName);
+        addProperty("domainname", domainName);
 
-	domainname = domainName;
-	hostname = hostName;
+        domainname = domainName;
+        hostname = hostName;
     }
 
     private void addProperty(String name, String value) {
-	if (getProject().getUserProperty(name) == null) {
-	    getProject().setInheritedProperty(name, value);
-	} else {
-	    log("Override ignored for " + name, Project.MSG_VERBOSE);
-	}
+        if (getProject().getUserProperty(name) == null) {
+            getProject().setInheritedProperty(name, value);
+        } else {
+            log("Override ignored for " + name, Project.MSG_VERBOSE);
+        }
     }
 
-
     public void setHostname(String name) {
-	this.hostname = name;
+        this.hostname = name;
     }
 
     public void setDomainname(String name) {
-	this.domainname = name;
+        this.domainname = name;
     }
 
 }
--- ant-tasks/MapLibraryName.java
+++ ant-tasks/MapLibraryName.java
@@ -5,65 +5,65 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.EnumeratedAttribute;
 
-
 public class MapLibraryName extends Task {
 
     private String property;
+
     private String name;
 
     private String outtype = "shared";
 
     public void execute() throws BuildException {
-	if (property == null) {
-	    throw new BuildException("MapLibraryName requires a \"property\" field");
-	}
-	if (name == null) {
-	    throw new BuildException("MapLibraryName requires a \"name\" field");
-	}
+        if (property == null) {
+            throw new BuildException(
+                    "MapLibraryName requires a \"property\" field");
+        }
+        if (name == null) {
+            throw new BuildException("MapLibraryName requires a \"name\" field");
+        }
 
-	String dirname = null;
-	String basename = name;
-	int slash = name.lastIndexOf(java.io.File.separatorChar);
-	if (slash != -1) {
-	    dirname = name.substring(0, slash);
-	    basename = name.substring(slash + 1);
-	}
-	String libName = basename;
-	if (outtype.equals("shared")) {
-	    libName = System.mapLibraryName(basename);
-	}
-	if (dirname != null) {
-	    libName = dirname + java.io.File.separator + libName;
-	}
-	addProperty(property, libName);
+        String dirname = null;
+        String basename = name;
+        int slash = name.lastIndexOf(java.io.File.separatorChar);
+        if (slash != -1) {
+            dirname = name.substring(0, slash);
+            basename = name.substring(slash + 1);
+        }
+        String libName = basename;
+        if (outtype.equals("shared")) {
+            libName = System.mapLibraryName(basename);
+        }
+        if (dirname != null) {
+            libName = dirname + java.io.File.separator + libName;
+        }
+        addProperty(property, libName);
     }
 
     private void addProperty(String name, String value) {
-	if (getProject().getUserProperty(name) == null) {
-	    getProject().setInheritedProperty(name, value);
-	} else {
-	    log("Override ignored for " + name, Project.MSG_VERBOSE);
-	}
+        if (getProject().getUserProperty(name) == null) {
+            getProject().setInheritedProperty(name, value);
+        } else {
+            log("Override ignored for " + name, Project.MSG_VERBOSE);
+        }
     }
 
     public void setProperty(String property) {
-	this.property = property;
+        this.property = property;
     }
 
     public void setName(String name) {
-	this.name = name;
+        this.name = name;
     }
 
     public void setOuttype(OutputTypeEnum outputType) {
-	this.outtype = outputType.getValue();
+        this.outtype = outputType.getValue();
     }
-
 
     public static class OutputTypeEnum extends EnumeratedAttribute {
 
-	public String[] getValues() {
-	    return new String[] { "shared", "static" };
-	}
+        public String[] getValues() {
+            return new String[] { "shared", "static" };
+        }
 
     }
 
