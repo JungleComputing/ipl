56cca34cdf7d51789247ada89f5c6bd8c910c64d
Jason Maassen <j.maassen@esciencecenter.nl>
2005-10-27 08:13:20 +0000

    Added new files.
    J.

---
+++ src/ibis/gmi/GroupCallHandler.java
@@ -0,0 +1,211 @@
+package ibis.gmi;
+
+import ibis.ipl.ReadMessage;
+import ibis.ipl.Upcall;
+
+/**
+ * Handler class for all upcalls.
+ */
+final class GroupCallHandler implements GroupProtocol, Upcall {
+
+    /**
+     * Invocation handler.
+     *
+     * @param r  the message to be read
+     */
+    private void handleInvocation(ReadMessage r) throws Exception {
+
+        int dest = r.readInt();
+        int inv = r.readByte();
+        int res = r.readByte();
+
+        switch (inv) {
+        case InvocationScheme.I_GROUP:
+        case InvocationScheme.I_COMBINED_FLAT_GROUP: {
+            GroupSkeleton s = Group.getSkeletonByGroupID(dest);
+
+            if (Group.logger.isDebugEnabled()) {
+                Group.logger.debug(Group._rank + 
+                        ": GroupCallHandler.handleInvocation() - "
+                        + "It is a GROUP INVOCATION");
+                Group.logger.debug(Group._rank + ": skeleton = " + s);
+            }
+
+            s.handleMessage(inv, res, r);
+        }
+            break;
+        default: {
+            GroupSkeleton s = Group.getSkeleton(dest);
+            s.handleMessage(inv, res, r);
+        }
+            break;
+        }
+    }
+
+    /**
+     * Upcall method. Handle the upcall, divide and conquer.
+     *
+     * @param m  the message to be read.
+     */
+    public void upcall(ReadMessage m) {
+
+        int ticket;
+        GroupSkeleton s;
+        byte opcode;
+        byte resultMode;
+
+        try {
+            opcode = m.readByte();
+
+            switch (opcode) {
+            case GroupProtocol.REGISTRY:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a REGISTRY");
+                }
+
+                Group.registry.handleMessage(m);
+                break;
+
+            case GroupProtocol.INVOCATION:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got an INVOCATION");
+                }
+
+                handleInvocation(m);
+                break;
+
+            case GroupProtocol.REGISTRY_REPLY:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank
+                            + ": GroupCallHandler.upcall() - "
+                            + "Got a REGISTRY_REPLY");
+                }
+
+                ticket = m.readInt();
+                RegistryReply r = (RegistryReply) m.readObject();
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - " +
+                            "REGISTRY_REPLY forwarded to ticketMaster (" +
+                            ticket + ")");
+                }
+
+                m.finish(); // ticketMaster may block.
+                Group.logger.debug(Group._rank
+                        + ": REGISTRY_REPLY forwarded to ticketMaster ("
+                        + ticket + ")");
+                Group.ticketMaster.put(ticket, r);
+                break;
+
+            case GroupProtocol.INVOCATION_FLATCOMBINE:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a INVOCATION_FLATCOMBINE");
+                }
+
+                Group.getGroupStub(m.readInt()).handleFlatInvocationCombineMessage(
+                        m);
+                break;
+
+            case GroupProtocol.INVOCATION_BINCOMBINE:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - " +
+                            "Got a INVOCATION_BINCOMBINE");
+                }
+
+                Group.getGroupStub(m.readInt()).handleBinInvocationCombineMessage(
+                        m);
+                break;
+
+            case GroupProtocol.INVOCATION_REPLY:
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - " +
+                            "Got a INVOCATION_REPLY");
+                }
+
+                resultMode = m.readByte();
+                ticket = m.readInt();
+                int stub = ticket >> 16;
+                ticket = ticket & 0xFFFF;
+                
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - " +
+                            "INVOCATION_REPLY forwarded to stub (" + 
+                            stub + ", " + ticket + ")");
+                }
+
+                Group.getGroupStub(stub).handleResultMessage(m,
+                        ticket, resultMode);
+                break;
+
+            case GroupProtocol.COMBINE:
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a COMBINE");
+                }
+
+                s = Group.getSkeleton(m.readInt());
+                s.handleCombineMessage(m);
+                break;
+
+            case GroupProtocol.COMBINE_RESULT:
+
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a COMBINE_RESULT");
+                }
+                
+                s = Group.getSkeletonByGroupID(m.readInt());
+                s.handleCombineMessage(m);
+                break;
+                
+            case GroupProtocol.CREATE_MULTICAST_PORT:
+                
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a CREATE_MULTICAST_PORT");
+                }
+                
+                MulticastGroups.handleCreateMulticastReceivePort(m);
+                break;                
+                
+            case GroupProtocol.CREATE_MULTICAST_PORT_REPLY:
+                
+                if (Group.logger.isDebugEnabled()) {
+                    Group.logger.debug(Group._rank + 
+                            ": GroupCallHandler.upcall() - "
+                            + "Got a CREATE_MULTICAST_PORT_REPLY");
+                }
+                                
+                MulticastGroups.handleCreateMulticastReceivePortReply(m);
+                break;                
+                                
+            default:                    
+                Group.logger.warn(Group._rank + 
+                        ": GroupCallHandler.upcall() - "
+                        + "Got an illegal opcode !");
+            }
+        } catch (Exception e) {
+            Group.logger.warn(Group._rank + 
+                    ": GroupCallHandler.upcall() - " +
+                    ": Got an exception in GroupCallHandler !", e);                
+        }
+    }
+}
---
+++ src/ibis/gmi/MulticastGroups.java
@@ -0,0 +1,258 @@
+package ibis.gmi;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+
+import ibis.ipl.ReceivePortIdentifier;
+import ibis.ipl.PortType;
+import ibis.ipl.ReadMessage;
+import ibis.ipl.WriteMessage;
+
+import ibis.ipl.SendPort;
+import ibis.ipl.ReceivePort;
+import ibis.util.GetLogger;
+
+class MulticastGroups {
+    
+    public static Logger logger = GetLogger.getLogger(Group.class.getName());
+    
+    private static final HashMap receivePorts = new HashMap();    
+    private static final HashMap sendPorts = new HashMap();
+    
+    private static PortType portType;
+            
+    static void init(PortType type) { 
+        portType = type;
+    }
+    
+    static void handleCreateMulticastReceivePort(ReadMessage m) { 
+                        
+        try {         
+            int rank = m.readInt();
+            int ticket = m.readInt();
+            String id = m.readString();
+            m.finish();
+        
+            if (logger.isDebugEnabled()) { 
+                logger.debug(Group._rank  
+                        + ": MulticastGroups.handleCreateMulticastReceivePort - " 
+                        + "Request for multicast receive port " + id);
+            }            
+                                    
+            // sanity check 
+            synchronized (receivePorts) {
+                if (receivePorts.containsKey(id)) { 
+//                  should never happen -- internal error ?
+                    Group.logger.fatal(Group._rank 
+                        + ": MulticastGroup.handleCreateMulticastReceivePort()"
+                        + " - : Attempt to create duplicate group " + id);
+                }
+            }
+
+            // TODO: Use anonymous ports instead ?
+            String myID = id + "-" + Group._rank;
+            
+            if (logger.isDebugEnabled()) { 
+                logger.debug(Group._rank  
+                        + ": MulticastGroups.handleCreateMulticastReceivePort - " 
+                        + "Creating multicast receive port " + myID);
+            }            
+            
+            ReceivePort rp = portType.createReceivePort(myID, 
+                    new GroupCallHandler());
+            rp.enableConnections();
+            rp.enableUpcalls();
+                                    
+            synchronized (receivePorts) {
+                receivePorts.put(id, rp);
+            }
+                
+            WriteMessage wm = Group.unicast[rank].newMessage();
+            
+            wm.writeByte(GroupProtocol.CREATE_MULTICAST_PORT_REPLY);
+            wm.writeInt(ticket);
+            wm.writeObject(rp.identifier());
+            wm.finish();
+
+            if (logger.isDebugEnabled()) { 
+                logger.debug(Group._rank  
+                        + ": MulticastGroups.handleCreateMulticastReceivePort - " 
+                        + "Multicast receive port " + myID + " done");
+            }            
+            
+        } catch (Exception e) {
+            logger.fatal(Group._rank + 
+                    ": MulticastGroup.handleCreateMulticastReceivePort()"
+                    + " - : Got an exception ", e);           
+        }
+    }
+    
+    static void handleCreateMulticastReceivePortReply(ReadMessage m) {
+        
+        try { 
+            int ticket = m.readInt();
+            Object id = m.readObject();            
+            Group.ticketMaster.put(ticket, id);
+            // shouldn't have to finish the message here ....
+            
+            if (logger.isDebugEnabled()) { 
+                logger.debug(Group._rank  
+                        + ": MulticastGroups.handleCreateMulticastReceivePortReply - " 
+                        + "Received multicast reply [" + ticket + "] - " + id);
+            }            
+            
+        } catch (Exception e) {
+            logger.fatal(Group._rank + 
+                    ": MulticastGroup.handleCreateMulticastReceivePortReply()"
+                    + " - : Got an exception ", e);           
+        }        
+    }     
+    
+    static SendPort getMulticastSendport(String ID, int[] hosts) {
+                
+        // First translate ID so it is unique accros machines 
+        ID = Group._rank + "-" + ID;
+        
+        if (logger.isDebugEnabled()) { 
+            logger.debug(Group._rank + ": MulticastGroups.getMulticastSendport"
+                    + " - Looking for multicast sendport " + ID);
+        }
+         
+        // NOT USED 
+        //if (hosts.length == 1) {
+            //return unicast[hosts[0]];
+        //}
+                        
+        // First check if we already have one a sendport to this group...
+        synchronized (sendPorts) {            
+            SendPort s = (SendPort) sendPorts.get(ID);
+            if (s != null) {
+                
+                if (logger.isDebugEnabled()) { 
+                    logger.debug(Group._rank  
+                            + ": MulticastGroups.getMulticastSendport - " 
+                            + "Found existing multicast sendport " + ID);
+                }
+                                
+                return s;
+            }
+        }
+        
+        if (logger.isDebugEnabled()) { 
+            logger.debug(Group._rank  
+                    + ": MulticastGroups.getMulticastSendport - " 
+                    + "Creating new multicast sendport " + ID);
+        }
+                
+        // If not, create one by sending a message to all machines that will 
+        // be part of the group...
+        try {
+                        
+            int [] tickets = new int[hosts.length];        
+                       
+            // Send all messages
+            for (int i=0;i<hosts.length;i++) { 
+                tickets[i] = Group.ticketMaster.get();
+                
+                WriteMessage wm = Group.unicast[hosts[i]].newMessage();
+                wm.writeByte(GroupProtocol.CREATE_MULTICAST_PORT);
+                wm.writeInt(Group._rank);
+                wm.writeInt(tickets[i]);
+                wm.writeString(ID);
+                wm.finish();
+                
+                if (logger.isDebugEnabled()) { 
+                    logger.debug(Group._rank  
+                            + ": MulticastGroups.getMulticastSendport - " 
+                            + "Send request [" + tickets[i] + "]" 
+                            + " to " + hosts[i] );
+                }                                
+            }
+
+            // Collect all replies
+            ReceivePortIdentifier [] rids = new ReceivePortIdentifier[hosts.length];
+            
+            for (int i=0;i<tickets.length;i++) {
+
+                if (logger.isDebugEnabled()) { 
+                    logger.debug(Group._rank  
+                            + ": MulticastGroups.getMulticastSendport - " 
+                            + "Waiting for reply [" + tickets[i] + "]"); 
+                }
+
+                Object tpm = Group.ticketMaster.get(tickets[i]);
+                
+                if (logger.isDebugEnabled()) { 
+                    logger.debug(Group._rank  
+                            + ": MulticastGroups.getMulticastSendport - " 
+                            + "Got for reply [" + tickets[i] + "] " + tpm); 
+                }
+               
+                rids[i] = (ReceivePortIdentifier) tpm;
+                //Group.ticketMaster.get(tickets[i]);
+            }
+            
+            // Create a sendport
+            SendPort sp = portType.createSendPort(); 
+            
+            // Connect sendport to all receiveports
+            for (int i=0;i<rids.length;i++) {
+                sp.connect(rids[i]);                               
+            }
+            
+            // Store the SendPort for future use
+            synchronized (sendPorts) {            
+                sendPorts.put(ID, sp);
+            }
+
+            if (logger.isDebugEnabled()) { 
+                logger.debug(Group._rank  
+                        + ": MulticastGroups.getMulticastSendport - " 
+                        + "New multicast sendport " + ID + " created");
+            }
+                                   
+            // Done
+            return sp;
+            
+        } catch (Exception e) {
+            logger.fatal(Group._rank + 
+                    ": MulticastGroup.getMulticastSendPort()"
+                    + " - : Got an exception", e);           
+        }     
+        
+        // stupid compiler
+        return null;
+    }    
+    
+    static void exit() { 
+        
+        // Close all send and receive ports...
+        synchronized (sendPorts) {
+            Iterator ports = sendPorts.values().iterator();
+
+            while (ports.hasNext()) {
+                SendPort sp = (SendPort) (ports.next());
+                try { 
+                    sp.close();
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        } 
+        
+        synchronized (receivePorts) {
+            Iterator ports = receivePorts.values().iterator();
+
+            while (ports.hasNext()) {
+                ReceivePort rp = (ReceivePort) (ports.next());
+                try { 
+                    rp.close();
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+            }
+        }         
+    }
+}
