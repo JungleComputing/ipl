8ed43ffeb26f6270329a7c4f5feab428e68d2459
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2006-01-10 15:43:06 +0000

    Added comments

--- src/ibis/satin/so/SharedObject.java
+++ src/ibis/satin/so/SharedObject.java
@@ -2,15 +2,28 @@ package ibis.satin.so;
 
 import ibis.satin.impl.Satin;
 
+/**
+ * A Satin shared object must be of a class that extends this
+ * <code>SharedObject</code> class.
+ * This class exports one method that actually marks the object as shared.
+ */
 public class SharedObject implements java.io.Serializable {
 
+    /**
+     * Identification of this shared object. Must be public because it is
+     * accessed from code that is rewritten by Satinc.
+     */
     public String objectId;
 
-    static int sharedObjectsCounter = 0;
+    /** Counter for generating shared-object identifications. */
+    private static int sharedObjectsCounter = 0;
 
     protected SharedObject() {
     }
 
+    /**
+     * Creates an identification for the current object and marks it as shared.
+     */
     public void exportObject() {
         Satin satin = Satin.getSatin();
 
--- src/ibis/satin/so/WriteMethodsInterface.java
+++ src/ibis/satin/so/WriteMethodsInterface.java
@@ -1,3 +1,11 @@
 package ibis.satin.so;
 
+/**
+ * This interface is a marker interface for methods that change the value
+ * of a Satin shared object.
+ * A shared object that has methods that change the value of the object
+ * must mark these methods as write methods. This is accomplished by
+ * putting these methods in an interface that extends this marker interface.
+ * The shared object class must implement this interface.
+ */
 public interface WriteMethodsInterface {};
