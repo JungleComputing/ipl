87fc8bbd7381d2e1e045a08cb5c6fb3e7cfec21a
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2006-01-11 12:39:34 +0000

    removed threshold from Fib

--- apps/satin/fib/Fib.java
+++ apps/satin/fib/Fib.java
@@ -1,39 +1,49 @@
 /* $Id$ */
 
-final class Fib extends ibis.satin.SatinObject implements FibInterface,
-        java.io.Serializable {
-
-    int threshold;
-
-    public Fib(int threshold) {
-        this.threshold = threshold;
-    }
-
-    public long fib_seq(int n) {
+/*
+ * This is one of the most simple Satin programs possible. It calculates
+ * Fibonacci numbers. It calculates them in a very inefficient way, but this
+ * program is used as an example and as a benchmark. It is not trying to be a
+ * fast Fibonacci implementation.
+ */
+final class Fib extends ibis.satin.SatinObject implements FibInterface {
+    /**
+     * Sequential version, used for comparison
+     * 
+     * @param n
+     * @return fib(n)
+     */
+    public long fibSeq(int n) {
         if (n < 2)
             return n;
-        return fib_seq(n - 1) + fib_seq(n - 2);
+
+        return fibSeq(n - 1) + fibSeq(n - 2);
     }
 
+    /**
+     * Parallel Satin version
+     * 
+     * @param n
+     * @return fib(n)
+     */
     public long fib(int n) {
-        long x, y;
-
-        //		System.err.println("running fib " + n);
-
         if (n < 2)
             return n;
 
-        if (n < threshold) {
-            return fib_seq(n);
-        }
-
-        x = fib(n - 1);
-        y = fib(n - 2);
+        long x = fib(n - 1);
+        long y = fib(n - 2);
         sync();
+
         return x + y;
     }
 
-    static long fib_iter(int n) {
+    /**
+     * Much more efficient iterative version
+     * 
+     * @param n
+     * @return fib(n)
+     */
+    static long fibIter(int n) {
         long f0, f1, f2;
         int i;
         if (n < 2) {
@@ -52,47 +62,45 @@ final class Fib extends ibis.satin.SatinObject implements FibInterface,
     }
 
     public static void main(String[] args) {
-        long res, res2;
+        long resSeq, resSatin;
         int n = 0;
 
         if (args.length == 0) {
             n = 28;
-        } else if (args.length > 2) {
-            System.out.println("Usage: fib <n> [ <threshold> ]");
+        } else if (args.length > 1) {
+            System.out.println("Usage: fib <n>");
             System.exit(1);
         } else {
             n = Integer.parseInt(args[0]);
         }
-        int threshold = 0;
 
-        if (args.length == 2) {
-            threshold = Integer.parseInt(args[1]);
-        }
+        Fib f = new Fib();
 
-        Fib f = new Fib(threshold);
-
-        System.out.println("Running Fib " + n + ", threshold = " + threshold);
-        res2 = fib_iter(n);
+        System.out.println("Running Fib " + n);
 
         long start = System.currentTimeMillis();
-        res = f.fib(n);
+        resSeq = f.fibSeq(n);
+        double timeSeq = (double) (System.currentTimeMillis() - start) / 1000.0;
+        System.err.println("Sequential version took "  + timeSeq + " s");
+        
+        start = System.currentTimeMillis();
+        resSatin = f.fib(n);
         f.sync();
-        double time = (double) (System.currentTimeMillis() - start) / 1000.0;
+        double timeSatin = (double) (System.currentTimeMillis() - start) / 1000.0;
 
-        if (res != res2) {
+        if (resSatin != resSeq) {
             System.out.println("application result fib GAVE WRONG RESULT! "
-                    + res + " should be " + res2);
-            if (args.length == 0) {
-                System.out.println("Test failed!");
-                System.exit(1);
-            }
+                    + resSatin + " should be " + resSeq);
+            System.out.println("Test failed!");
+            System.exit(1);
         } else {
-            System.out.println("application time fib (" + n + ") took " + time
-                    + " s");
-            System.out.println("application result fib (" + n + ") = " + res);
-            if (args.length == 0) {
-                System.out.println("Test succeeded!");
-            }
+            System.out.println("application time fib (" + n + ") took "
+                    + timeSatin + " s");
+            System.out.println("application result fib (" + n + ") = "
+                    + resSatin);
+            
+            System.out.println("Satin overhead factor is: " + (timeSatin / timeSeq));
+            System.out.println("Test succeeded!");
         }
     }
 }
