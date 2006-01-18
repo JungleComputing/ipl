3cc366e460146796f9fa1b37e06426c1ca4e89dd
Gosia Wrezinska <gosia@cs.vu.nl>
2006-01-18 08:35:55 +0000

    Traveling Salesman Problem with the Younger Brother Wait optimization. No shared objects

---
+++ apps/satin/tsp_ybw/TspInterface.java
@@ -0,0 +1,6 @@
+/* $Id$ */
+
+interface TspInterface extends ibis.satin.Spawnable {
+    public int spawn_tsp(int hops, byte[] path, int length, int minimum,
+            DistanceTable distance);
+}
