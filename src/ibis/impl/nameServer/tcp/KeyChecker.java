5b6c85b8f0f2988ac8d23d1b4f00c66da84c18d6
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2006-04-20 13:54:39 +0000

    Removed unused import

--- src/ibis/impl/nameServer/tcp/KeyChecker.java
+++ src/ibis/impl/nameServer/tcp/KeyChecker.java
@@ -12,7 +12,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
 
