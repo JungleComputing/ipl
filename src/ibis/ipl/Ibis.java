eba6840984357209f1b935e860f95f7d0a40dfb7
Rob van Nieuwpoort <r.vannieuwpoort@esciencecenter.nl>
2006-09-07 15:14:42 +0000

    fixed broken imports

--- src/ibis/ipl/Ibis.java
+++ src/ibis/ipl/Ibis.java
@@ -6,7 +6,6 @@ import ibis.util.ClassLister;
 import ibis.util.IPUtils;
 import ibis.util.TypedProperties;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
