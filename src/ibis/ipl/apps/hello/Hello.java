0c4ae14e4223b24a655d0d80ab95df0d1d5eff89
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2007-09-25 07:24:36 +0000

    Added Hello example

---
+++ src/ibis/ipl/apps/hello/Hello.java
@@ -0,0 +1,90 @@
+/* $Id: Hello.java 6430 2007-09-20 16:37:59Z ceriel $ */
+
+package ibis.ipl.apps.hello;
+
+import ibis.ipl.Ibis;
+import ibis.ipl.IbisCapabilities;
+import ibis.ipl.IbisFactory;
+import ibis.ipl.IbisIdentifier;
+import ibis.ipl.PortType;
+import ibis.ipl.ReadMessage;
+import ibis.ipl.ReceivePort;
+import ibis.ipl.SendPort;
+import ibis.ipl.WriteMessage;
+
+import java.io.IOException;
+
+/**
+ * This program is to be run as two instances. One is a server, the other
+ * a client. The client sends a hello message to the server. The server
+ * prints it.
+ */
+
+public class Hello {
+
+    PortType portType = new PortType(
+            PortType.COMMUNICATION_RELIABLE, PortType.SERIALIZATION_DATA,
+            PortType.RECEIVE_EXPLICIT, PortType.CONNECTION_ONE_TO_ONE);
+
+    IbisCapabilities ibisCapabilities = new IbisCapabilities(
+            IbisCapabilities.CLOSEDWORLD,
+            IbisCapabilities.ELECTIONS_STRICT);
+
+    private void server(Ibis myIbis) throws IOException {
+
+        // Create a receive port and enable connections.
+        ReceivePort receiver = myIbis.createReceivePort(portType, "server");
+        receiver.enableConnections();
+
+        // Read the message.
+        ReadMessage r = receiver.receive();
+        String s = r.readString();
+        r.finish();
+        System.out.println("Server received: " + s);
+
+        // Close receive port.
+        receiver.close();
+    }
+
+    private void client(Ibis myIbis, IbisIdentifier server) throws IOException {
+
+        // Create a send port for sending requests and connect.
+        SendPort sender = myIbis.createSendPort(portType);
+        sender.connect(server, "server");
+
+        // Send the message.
+        WriteMessage w = sender.newMessage();
+        w.writeString("Hi there");
+        w.finish();
+
+        // Close ports.
+        sender.close();
+    }
+
+    private void run() throws Exception {
+        // Create an ibis instance.
+        Ibis ibis = IbisFactory.createIbis(ibisCapabilities, null, true,
+                null, portType);
+
+        // Elect a server
+        IbisIdentifier server = ibis.registry().elect("Server");
+
+        // If I am the server, run server, else run client.
+        if (server.equals(ibis.identifier())) {
+            server(ibis);
+        } else {
+            client(ibis, server);
+        }
+
+        // End ibis.
+        ibis.end();
+    }
+
+    public static void main(String args[]) {
+        try {
+            new Hello().run();
+        } catch (Exception e) {
+            System.err.println("Got exception: " + e);
+        }
+    }
+}
