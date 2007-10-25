32d8675705420e87da7646fc8949021649418326
Niels Drost <n.drost@esciencecenter.nl>
2007-10-25 11:23:25 +0000

    added dumb class

---
+++ src/ibis/ipl/impl/registry/gossip/ElectionSet.java
@@ -0,0 +1,35 @@
+package ibis.ipl.impl.registry.gossip;
+
+import ibis.ipl.impl.IbisIdentifier;
+import ibis.util.TypedProperties;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class ElectionSet {
+
+    public ElectionSet(TypedProperties properties, Registry registry) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void writeGossipData(DataOutputStream stream) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void readGossipData(DataInputStream stream) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public IbisIdentifier getElectionResult(long timeoutMillis) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public IbisIdentifier elect(String electionName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
