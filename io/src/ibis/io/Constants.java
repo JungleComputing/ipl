e0cc5333e3c45960b8d1f9865f03f775508fa97c
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2009-11-05 09:37:48 +0000

    Added just a little javadoc.

--- io/src/ibis/io/Constants.java
+++ io/src/ibis/io/Constants.java
@@ -1,5 +1,9 @@
 package ibis.io;
 
+/**
+ * Some constants for the {@code ibis.io} package.
+ * This class must be public because it is accessed by Ibisc-rewritten code.
+ */
 public final class Constants {
     public static final int TYPE_BIT = (1 << 31);
 
