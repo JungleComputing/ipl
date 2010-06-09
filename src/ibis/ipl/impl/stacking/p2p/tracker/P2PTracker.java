66a32cbeb4769715e9eeb3ee486dbf2e1baea471
Delia Arsinte <delia.arsinte@gmail.com>
2010-06-09 16:13:17 +0000

    - implemented tracker - fixed bugs at fault tolerance part

---
+++ src/ibis/ipl/impl/stacking/p2p/tracker/P2PTracker.java
@@ -0,0 +1,142 @@
+package ibis.ipl.impl.stacking.p2p.tracker;
+
+import ibis.ipl.Ibis;
+import ibis.ipl.IbisCapabilities;
+import ibis.ipl.IbisFactory;
+import ibis.ipl.IbisIdentifier;
+import ibis.ipl.MessageUpcall;
+import ibis.ipl.PortType;
+import ibis.ipl.ReadMessage;
+import ibis.ipl.ReceivePort;
+import ibis.ipl.WriteMessage;
+import ibis.ipl.SendPort;
+import ibis.ipl.impl.stacking.p2p.util.P2PConfig;
+import ibis.ipl.impl.stacking.p2p.util.P2PMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Random;
+import java.util.Vector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class P2PTracker implements MessageUpcall{
+
+	private Vector<IbisIdentifier> joinedPeers = new Vector<IbisIdentifier>();
+
+	IbisCapabilities ibisCapabilities = new IbisCapabilities(
+			IbisCapabilities.ELECTIONS_STRICT,
+			IbisCapabilities.MEMBERSHIP_TOTALLY_ORDERED);
+
+	Ibis myIbis;
+	ReceivePort receiver;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(P2PTracker.class);
+
+	public P2PTracker() {
+		try {
+			myIbis = IbisFactory.createIbis(ibisCapabilities, null, P2PConfig.portType);
+			receiver = myIbis.createReceivePort(P2PConfig.portType, P2PConfig.TRACKER_PORT,
+					this);
+
+			// enable connections
+			receiver.enableConnections();
+			// enable upcalls
+			receiver.enableMessageUpcalls();
+
+			// perform an election, annouce to the others that I am the tracker
+			myIbis.registry().elect(P2PConfig.ELECTION_TRACKER);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		//new Thread(this).start();
+	}
+
+	@Override
+	public void upcall(ReadMessage readMessage) throws IOException,
+			ClassNotFoundException {
+
+		P2PMessage msg = (P2PMessage) readMessage.readObject();
+
+		switch (msg.getType()) {
+		case P2PMessage.REGISTER_IBIS:
+			handleRegisterIbis(readMessage);
+			break;
+		case P2PMessage.GET_IBISES:
+			handleGetJoinedIbises(readMessage);
+			break;
+		}
+	}
+
+	private void handleGetJoinedIbises(ReadMessage readMessage)
+			throws IOException, ClassNotFoundException {
+		IbisIdentifier source = (IbisIdentifier) readMessage.readObject();
+		readMessage.finish();
+
+		// connect to source
+		SendPort sendPort = (SendPort) myIbis.createSendPort(P2PConfig.portType);
+		sendPort.connect(source, P2PConfig.TRACKER_PORT);
+
+		// select P2PConfig.NEARBY_REQUESTS random joined nodes
+		int nearbyRequests = Math.min(joinedPeers.size(), P2PConfig.NEARBY_REQUESTS);
+		ArrayList<IbisIdentifier> nearbyNodes = new ArrayList<IbisIdentifier>();
+		
+		BitSet set = new BitSet();
+		set.clear();
+
+		logger.debug("Nearby requests: " + nearbyRequests);
+		
+		Random random = new Random();
+		int selected = 0;
+		for (int i = 0; i < nearbyRequests; i++) {
+			// generate random numbers between 0 and no. of ibises
+			// until selected is not a not previously queried or my own id
+			while (set.get(selected)) {
+				selected = random.nextInt(nearbyRequests);
+				
+			}
+			nearbyNodes.add(joinedPeers.elementAt(selected));
+			set.set(selected);
+		}
+		
+		// send back joined Ibises
+		WriteMessage writeMessage = sendPort.newMessage();
+		writeMessage.writeObject(nearbyNodes);
+		writeMessage.finish();
+		sendPort.close();
+	}
+
+	private void handleRegisterIbis(ReadMessage readMessage)
+			throws IOException, ClassNotFoundException {
+		IbisIdentifier newIbis = (IbisIdentifier) readMessage.readObject();
+		readMessage.finish();
+
+		joinedPeers.add(newIbis);
+		
+		logger.debug("Registered node with IbisIdentifier " + newIbis.name());
+	}
+
+	public static void main(String args[]) {
+		P2PTracker tracker = new P2PTracker();
+		tracker.run();
+	}
+
+	private synchronized void waitFor(long time) {
+		try {
+			wait(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();		
+		}
+	}
+	
+	public void run() {
+		while (true) {
+			waitFor(5000);
+		}
+	}
+}
---
+++ src/ibis/ipl/impl/stacking/p2p/tracker/P2PTrackerClient.java
@@ -0,0 +1,94 @@
+package ibis.ipl.impl.stacking.p2p.tracker;
+
+import ibis.ipl.ConnectionFailedException;
+import ibis.ipl.Ibis;
+import ibis.ipl.IbisIdentifier;
+import ibis.ipl.ReadMessage;
+import ibis.ipl.ReceivePort;
+import ibis.ipl.SendPort;
+import ibis.ipl.WriteMessage;
+import ibis.ipl.impl.stacking.p2p.util.P2PConfig;
+import ibis.ipl.impl.stacking.p2p.util.P2PMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class P2PTrackerClient {
+	private SendPort sendPort;
+	private IbisIdentifier trackerID;
+	private Ibis baseIbis;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(P2PTrackerClient.class);
+
+	public P2PTrackerClient(Ibis ibis) throws IOException {
+		this.baseIbis = ibis;
+		initializeTracker();
+		
+		logger.debug("Tracker Client was initialized.");
+	}
+
+	public void sendNodeInfo() throws IOException {
+		P2PMessage msg = new P2PMessage(null, P2PMessage.REGISTER_IBIS);
+		WriteMessage writeMsg = sendPort.newMessage();
+		writeMsg.writeObject(msg);
+		writeMsg.writeObject(baseIbis.identifier());
+		writeMsg.finish();
+	}
+
+	/**
+	 * initialize tracker
+	 * @throws IOException
+	 */
+	private void initializeTracker() throws IOException {
+		trackerID = baseIbis.registry().getElectionResult(
+				P2PConfig.ELECTION_TRACKER);
+		logger.debug("Tracker is located at " + trackerID.name() + " .");
+		
+		setSendPort(baseIbis.createSendPort(P2PConfig.portType));
+	}
+
+	/**
+	 * @param sendPort
+	 *            the sendPort to set
+	 * @throws ConnectionFailedException
+	 */
+	public synchronized void setSendPort(SendPort sendPort)
+			throws ConnectionFailedException {
+		this.sendPort = sendPort;
+		this.sendPort.connect(trackerID, P2PConfig.TRACKER_PORT);
+	}
+
+	/**
+	 * get joined ibises from tracker
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@SuppressWarnings("unchecked")
+	public ArrayList<IbisIdentifier> getJoinedIbises() throws IOException,
+			ClassNotFoundException {
+		P2PMessage msg = new P2PMessage(null, P2PMessage.GET_IBISES);
+		WriteMessage writeMsg = sendPort.newMessage();
+		writeMsg.writeObject(msg);
+		writeMsg.writeObject(baseIbis.identifier());
+		writeMsg.finish();
+
+		logger.debug("Sent request for joined Ibises.");
+		
+		ReceivePort receiver = baseIbis.createReceivePort(P2PConfig.portType,
+				P2PConfig.TRACKER_PORT);
+		receiver.enableConnections();
+
+		ReadMessage readMessage = receiver.receive();
+		ArrayList<IbisIdentifier> joinedIbises = (ArrayList<IbisIdentifier>) readMessage
+				.readObject();
+		
+		logger.debug("Received joined Ibises.");
+		
+		return joinedIbises;
+	}
+}
