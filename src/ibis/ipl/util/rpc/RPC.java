074a0a563d2cd98790fe6460fce9cd8fba508078
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2010-10-26 13:01:19 +0000

    Fixed Ibis serialization, so now SERIALIZATION_OBJECT instead of SERIALIZATION_OBJECT_SUN.

--- src/ibis/ipl/util/rpc/RPC.java
+++ src/ibis/ipl/util/rpc/RPC.java
@@ -14,14 +14,14 @@ public class RPC {
 	 * Port type used for sending a request to the server.
 	 */
 	public static final PortType rpcRequestPortType = new PortType(
-			PortType.COMMUNICATION_RELIABLE, PortType.SERIALIZATION_OBJECT_SUN,
+			PortType.COMMUNICATION_RELIABLE, PortType.SERIALIZATION_OBJECT,
 			PortType.RECEIVE_AUTO_UPCALLS, PortType.CONNECTION_MANY_TO_ONE);
 
 	/**
 	 * Port type used for sending a reply back.
 	 */
 	public static final PortType rpcReplyPortType = new PortType(
-			PortType.COMMUNICATION_RELIABLE, PortType.SERIALIZATION_OBJECT_SUN,
+			PortType.COMMUNICATION_RELIABLE, PortType.SERIALIZATION_OBJECT,
 			PortType.RECEIVE_EXPLICIT, PortType.CONNECTION_ONE_TO_ONE);
 
 	public static final PortType[] rpcPortTypes = { rpcRequestPortType,
