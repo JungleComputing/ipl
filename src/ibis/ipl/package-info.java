c44ef70acafb98f59be050c500fd4523e8f2a96f
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2012-04-04 12:20:29 +0000

    Updated a bit

--- src/ibis/ipl/package-info.java
+++ src/ibis/ipl/package-info.java
@@ -10,7 +10,11 @@
  * Only receive ports and send ports of the same port type can communicate. Any number of receive ports and
  * send ports can be created on a JVM (even of the same port type).
  * <p>
- * SendPorts and ReceivePorts are created by their Ibis instance using the
+ * First, an {@link ibis.ipl.Ibis Ibis} instance must be created. This is achieved by
+ * calling one of the <code>createIbis</code> methods of the {@link ibis.ipl.IbisFactory IbisFactory},
+ * specifying the {@link ibis.ipl.IbisCapabilities capabilities} and port types required.
+ * <p>
+ * SendPorts and ReceivePorts are created by an Ibis instance using the
  * {@link ibis.ipl.Ibis#createSendPort(PortType, String) createSendPort} and
  * {@link ibis.ipl.Ibis#createReceivePort(PortType, String) createReceivePort} methods. When creating a
  * ReceivePort, it can be supplied with an {@link ibis.ipl.MessageUpcall MessageUpcall} object. If so, upcalls
@@ -21,16 +25,18 @@
  * are implementation specific and serializable (and can be sent over the network/saved in a file etc.).
  * <p>
  * A SendPort takes the initiative to connect to or disconnect from ReceivePorts (otherwise the one-way traffic
- * scheme is violated). A SendPort can be connected to one or more ReceivePorts using their ReceivePortIdentifiers
- * or their Ibis identifiers and names. Additional ReceivePorts may be connected at any time. A SendPort can be
- * disconnected from one or more ReceivePorts using their ReceivePortIdentifiers. Additional ReceivePorts may be
- * disconnected at any time. When a SendPort is no longer used it must be closed using the
- * {@link ibis.ipl.SendPort#close() close} method. All connections the SendPort has are disconnected. When a
- * ReceivePort is no longer used it must be closed using the {@link ibis.ipl.ReceivePort#close() close} method.
+ * scheme is violated). A SendPort can connect to one or more ReceivePorts using their ReceivePortIdentifiers
+ * or their Ibis identifiers and names. Additional ReceivePorts may be connected to at any time, provided
+ * that the port type allows it. A SendPort can disconnect from one or more ReceivePorts using their
+ * ReceivePortIdentifiers. Additional ReceivePorts may be disconnected at any time.
+ * When a SendPort is no longer used it must be closed using the
+ * {@link ibis.ipl.SendPort#close() close} method. When this method is called, all connections the SendPort has
+ * are disconnected. When a ReceivePort is no longer used it must be closed using the
+ * {@link ibis.ipl.ReceivePort#close() close} method.
  * This call will block until connections to SendPorts are disconnected (by the SendPorts).
  * <p>
- * A {@link ibis.ipl.WriteMessage message} can be sent from an SendPort to the set of ReceivePorts it is connected to.
- * To do this, a {@link ibis.ipl.WriteMessage} write message is obtained from the SendPort (this allows streaming,
+ * A {@link ibis.ipl.WriteMessage message} can be sent from a SendPort to the set of ReceivePorts it is connected to.
+ * To do this, a {@link ibis.ipl.WriteMessage write message} is obtained from the SendPort (this allows streaming,
  * as the destination is known). Data can be added to the message using "write" methods (this data may be immediately
  * streamed to the ReceivePorts) of this message. The Ibis system may or may not asynchronously start sending the message.
  * The message can be finished using the {@link ibis.ipl.WriteMessage#finish() finish} method. When the finish returns,
@@ -44,6 +50,8 @@
  * {@link ibis.ipl.ReceivePort#receive() receive} method is called. When the message is no longer used it MUST be
  * returned to the system using the {@link ibis.ipl.ReadMessage#finish() finish} method (after which the message may
  * no longer be used). This allows the underlying implementation to deliver a next message.
+ * 
+ * All Ibis implementations must be thread-safe.
  */
 package ibis.ipl;
 
